45f1d59fcc5206e481d5b09541edcb01035cdddd
jost <>
2007-03-10 13:47:06 +0000

    Bugfix UNIQUEKEY

--- sql/update_0.4-0.5.sql
+++ sql/update_0.4-0.5.sql
@@ -1,6 +1,6 @@
 CREATE TABLE kursteilnehmer 
 (
-  id            INTEGER     default UNIQUEKEY('einmalabbu'), 
+  id            INTEGER     default UNIQUEKEY('kursteilnehmer'), 
   name          VARCHAR(27) NOT NULL, 
   vzweck1       VARCHAR(27) NOT NULL,
   vzweck2       VARCHAR(27),
@@ -11,6 +11,34 @@ CREATE TABLE kursteilnehmer
   geschlecht    VARCHAR(1),
   eingabedatum  DATE        NOT NULL,
   abbudatum     DATE,
+  UNIQUE        (id), 
+  PRIMARY KEY   (id)
+);
+ALTER CREATE TABLE mitglied 
+(
+  id            INTEGER     default UNIQUEKEY('mitglied'), 
+  anrede        VARCHAR(10),
+  titel         VARCHAR(10),
+  name          VARCHAR(40) NOT NULL, 
+  vorname       VARCHAR(40) NOT NULL, 
+  strasse       VARCHAR(40) NOT NULL, 
+  plz           VARCHAR(5)  NOT NULL, 
+  ort           VARCHAR(40) NOT NULL, 
+  blz           VARCHAR(8),
+  konto         VARCHAR(10),
+  kontoinhaber  VARCHAR(27),
+  geburtsdatum  DATE,
+  geschlecht    CHAR(1),
+  telefonprivat VARCHAR(15),
+  telefondienstlich VARCHAR(15),
+  email         VARCHAR(50),
+  eintritt      DATE,
+  beitragsgruppe INTEGER,
+  austritt      DATE,
+  kuendigung    DATE,
+  vermerk1      VARCHAR(255),
+  vermerk2      VARCHAR(255),
+  eingabedatum  DATE,
   UNIQUE        (id), 
   PRIMARY KEY   (id)
 );
