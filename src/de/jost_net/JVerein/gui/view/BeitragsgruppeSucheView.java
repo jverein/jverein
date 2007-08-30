53ac9033e9cc1626dbb5a359bb675beb43b15a25
jost <>
2007-08-30 19:49:18 +0000

    Löschung über Knopf

--- src/de/jost_net/JVerein/gui/view/BeitragsgruppeSucheView.java
+++ src/de/jost_net/JVerein/gui/view/BeitragsgruppeSucheView.java
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log$
+ * Revision 1.2  2007/02/23 20:27:28  jost
+ * Mail- und Webadresse im Header korrigiert.
+ *
  * Revision 1.1  2006/09/20 15:39:10  jost
  * *** empty log message ***
  *
@@ -16,6 +19,7 @@
 package de.jost_net.JVerein.gui.view;
 
 import de.jost_net.JVerein.gui.action.BackAction;
+import de.jost_net.JVerein.gui.action.BeitragsgruppeDeleteAction;
 import de.jost_net.JVerein.gui.action.BeitragsgruppeDetailAction;
 import de.jost_net.JVerein.gui.control.BeitragsgruppeControl;
 import de.willuhn.jameica.gui.AbstractView;
@@ -33,7 +37,9 @@ public class BeitragsgruppeSucheView extends AbstractView
 
     control.getBeitragsgruppeTable().paint(this.getParent());
 
-    ButtonArea buttons = new ButtonArea(this.getParent(), 2);
+    ButtonArea buttons = new ButtonArea(this.getParent(), 3);
+    buttons.addButton("Löschen", new BeitragsgruppeDeleteAction(), control
+        .getBeitragsgruppeTable());
     buttons.addButton("Neu", new BeitragsgruppeDetailAction());
     buttons.addButton("<< Zurück", new BackAction());
   }
