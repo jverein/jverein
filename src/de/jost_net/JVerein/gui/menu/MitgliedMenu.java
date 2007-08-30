24f5016c10053e4769a71d333af4b8af76d2ecb6
jost <>
2007-08-30 19:48:53 +0000

    Neues Kontext-Menü

---
+++ src/de/jost_net/JVerein/gui/menu/MitgliedMenu.java
@@ -0,0 +1,32 @@
+/**********************************************************************
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * Copyright (c) by Heiner Jostkleigrewe
+ * All rights reserved
+ * heiner@jverein.de
+ * www.jverein.de
+ * $Log$
+ **********************************************************************/
+package de.jost_net.JVerein.gui.menu;
+
+import de.jost_net.JVerein.gui.action.MitgliedDeleteAction;
+import de.willuhn.jameica.gui.parts.CheckedContextMenuItem;
+import de.willuhn.jameica.gui.parts.ContextMenu;
+
+/**
+ * Kontext-Menu zu den Beitragsgruppen.
+ */
+public class MitgliedMenu extends ContextMenu
+{
+
+  /**
+   * Erzeugt ein Kontext-Menu fuer die Liste der Mitglieder.
+   */
+  public MitgliedMenu()
+  {
+    addItem(new CheckedContextMenuItem("Löschen...", new MitgliedDeleteAction()));
+  }
+}
