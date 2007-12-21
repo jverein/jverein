2bcb60a2e5040dd8fb64344c79c9da83d690cb9a
jost <>
2007-12-21 14:49:39 +0000

    PDF-Dokumentation -> Wiki

---
+++ src/de/jost_net/JVerein/gui/action/DokumentationAction.java
@@ -0,0 +1,49 @@
+/**********************************************************************
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * Copyright (c) by Heiner Jostkleigrewe
+ * All rights reserved
+ * heiner@jverein.de
+ * www.jverein.de
+ * $Log$
+ * Revision 1.1  2007/07/20 20:15:19  jost
+ * Neu
+ *
+ **********************************************************************/
+package de.jost_net.JVerein.gui.action;
+
+import java.io.File;
+
+import de.willuhn.jameica.gui.Action;
+import de.willuhn.jameica.gui.GUI;
+import de.willuhn.jameica.gui.internal.action.Program;
+import de.willuhn.jameica.messaging.StatusBarMessage;
+import de.willuhn.jameica.system.Application;
+import de.willuhn.util.ApplicationException;
+
+public class DokumentationAction implements Action
+{
+  public void handleAction(Object context) throws ApplicationException
+  {
+    GUI.getDisplay().asyncExec(new Runnable()
+    {
+      public void run()
+      {
+        try
+        {
+          new Program().handleAction(new File(
+              "http://www.jverein.de/index.php5?title=Dokumentation"));
+        }
+        catch (ApplicationException ae)
+        {
+          Application.getMessagingFactory().sendMessage(
+              new StatusBarMessage(ae.getLocalizedMessage(),
+                  StatusBarMessage.TYPE_ERROR));
+        }
+      }
+    });
+  }
+}
