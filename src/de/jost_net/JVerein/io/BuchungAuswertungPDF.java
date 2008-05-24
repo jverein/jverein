128c3dd19b4cb0ee404c7994502fc85f966b4350
jost <>
2008-05-24 19:32:42 +0000

    Auswertung überarbeitet.

--- src/de/jost_net/JVerein/io/BuchungAuswertungPDF.java
+++ src/de/jost_net/JVerein/io/BuchungAuswertungPDF.java
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log$
+ * Revision 1.6  2008/03/16 07:37:37  jost
+ * Reaktivierung BuchfÃ¼hrung
+ *
  * Revision 1.4  2007/02/23 20:28:04  jost
  * Mail- und Webadresse im Header korrigiert.
  *
@@ -35,29 +38,19 @@ import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.Date;
 
-import com.lowagie.text.Chunk;
-import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
-import com.lowagie.text.Font;
 import com.lowagie.text.FontFactory;
-import com.lowagie.text.HeaderFooter;
-import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
-import com.lowagie.text.Phrase;
-import com.lowagie.text.pdf.PdfPCell;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfWriter;
 
 import de.jost_net.JVerein.Einstellungen;
-import de.jost_net.JVerein.JVereinPlugin;
 import de.jost_net.JVerein.rmi.Buchung;
 import de.jost_net.JVerein.rmi.Buchungsart;
+import de.jost_net.JVerein.rmi.Konto;
 import de.willuhn.datasource.rmi.DBIterator;
 import de.willuhn.jameica.gui.GUI;
 import de.willuhn.jameica.gui.internal.action.Program;
 import de.willuhn.jameica.messaging.StatusBarMessage;
-import de.willuhn.jameica.plugin.AbstractPlugin;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.logging.Logger;
 import de.willuhn.util.ApplicationException;
@@ -68,65 +61,33 @@ public class BuchungAuswertungPDF
   private double summe = 0;
 
   public BuchungAuswertungPDF(DBIterator list, final File file,
-      ProgressMonitor monitor, Date dVon, Date dBis)
-      throws ApplicationException, RemoteException
+      ProgressMonitor monitor, Konto konto, Buchungsart buchungsart, Date dVon,
+      Date dBis) throws ApplicationException, RemoteException
   {
-    Document rpt = null;
-
     try
     {
-      // ////////////////////////////////////////////////////////////////////////
-      // Header erzeugen
-      rpt = new Document(PageSize.A4, 80, 60, 60, 60);
-
       FileOutputStream fos = new FileOutputStream(file);
-      PdfWriter.getInstance(rpt, fos);
-      rpt.setMargins(50, 10, 50, 30); // links, rechts, oben, unten
-
-      AbstractPlugin plugin = Application.getPluginLoader().getPlugin(
-          JVereinPlugin.class);
-      rpt.addAuthor("JVerein - Version " + plugin.getManifest().getVersion());
       String subtitle = "vom " + Einstellungen.DATEFORMAT.format(dVon)
           + " bis " + Einstellungen.DATEFORMAT.format(dBis);
-      rpt.addTitle(subtitle);
-      // ////////////////////////////////////////////////////////////////////////
+      if (konto != null)
+      {
+        subtitle += " für Konto " + konto.getNummer() + " - "
+            + konto.getBezeichnung();
+      }
+      Reporter reporter = new Reporter(fos, monitor, "Buchungsliste", subtitle,
+          list.size());
 
-      // ////////////////////////////////////////////////////////////////////////
-      // Footer erzeugen
-      Chunk fuss = new Chunk("Ausgegeben am "
-          + Einstellungen.TIMESTAMPFORMAT.format(new Date())
-          + "              Seite:  ", FontFactory.getFont(
-          FontFactory.HELVETICA, 8, Font.BOLD));
-      HeaderFooter hf = new HeaderFooter(new Phrase(fuss), true);
-      hf.setAlignment(Element.ALIGN_CENTER);
-      rpt.setFooter(hf);
-
-      rpt.open();
-
-      Paragraph pTitle = new Paragraph("Buchungen", FontFactory.getFont(
-          FontFactory.HELVETICA_BOLD, 13));
-      pTitle.setAlignment(Element.ALIGN_CENTER);
-      rpt.add(pTitle);
-      Paragraph psubTitle = new Paragraph(subtitle, FontFactory.getFont(
-          FontFactory.HELVETICA_BOLD, 10));
-      psubTitle.setAlignment(Element.ALIGN_CENTER);
-      rpt.add(psubTitle);
-      // ////////////////////////////////////////////////////////////////////////
-
-      PdfPTable table = null;
       while (list.hasNext())
       {
-        table = getTableWithHeader();
-        createTableContent(rpt, list, table, dVon, dBis);
+        createTableContent(reporter, list, konto, dVon, dBis);
       }
-      createTableContent(rpt, null, table, dVon, dBis);
-
+      if (buchungsart.getArt() == -1)
+      {
+        createTableContent(reporter, null, konto, dVon, dBis);
+      }
       monitor.setStatusText("Auswertung fertig. " + list.size() + " Sätze.");
 
-      if (rpt != null)
-      {
-        rpt.close();
-      }
+      reporter.close();
       fos.close();
       GUI.getDisplay().asyncExec(new Runnable()
       {
@@ -164,77 +125,24 @@ public class BuchungAuswertungPDF
 
   }
 
-  /**
-   * Erzeugt eine Zelle der Tabelle.
-   * 
-   * @param text
-   *          der anzuzeigende Text.
-   * @param align
-   *          die Ausrichtung.
-   * @param backgroundcolor
-   *          die Hintergundfarbe.
-   * @return die erzeugte Zelle.
-   */
-  private PdfPCell getDetailCell(String text, int align, Color backgroundcolor)
+  private void createTableHeader(Reporter reporter) throws DocumentException
   {
-    PdfPCell cell = new PdfPCell(new Phrase(notNull(text), FontFactory.getFont(
-        FontFactory.HELVETICA, 8)));
-    cell.setHorizontalAlignment(align);
-    cell.setBackgroundColor(backgroundcolor);
-    return cell;
+    reporter.addHeaderColumn("Datum", Element.ALIGN_CENTER, 40,
+        Color.LIGHT_GRAY);
+    reporter.addHeaderColumn("Name", Element.ALIGN_CENTER, 100,
+        Color.LIGHT_GRAY);
+    reporter.addHeaderColumn("Zahlungsgrund", Element.ALIGN_CENTER, 100,
+        Color.LIGHT_GRAY);
+    reporter.addHeaderColumn("Zahlungsgrund2", Element.ALIGN_CENTER, 100,
+        Color.LIGHT_GRAY);
+    reporter.addHeaderColumn("Betrag", Element.ALIGN_CENTER, 60,
+        Color.LIGHT_GRAY);
+    reporter.createHeader();
+
   }
 
-  /**
-   * Erzeugt eine Zelle der Tabelle.
-   * 
-   * @param text
-   *          der anzuzeigende Text.
-   * @param align
-   *          die Ausrichtung.
-   * @return die erzeugte Zelle.
-   */
-  private PdfPCell getDetailCell(String text, int align)
-  {
-    return getDetailCell(text, align, Color.WHITE);
-  }
-
-  /**
-   * Gibt einen Leerstring aus, falls der Text null ist.
-   * 
-   * @param text
-   *          der Text.
-   * @return der Text oder Leerstring - niemals null.
-   */
-  private String notNull(String text)
-  {
-    return text == null ? "" : text;
-  }
-
-  private PdfPTable getTableWithHeader() throws DocumentException
-  {
-    PdfPTable table = new PdfPTable(5);
-    float[] widths = { 40, 100, 100, 100, 60 };
-    table.setWidths(widths);
-    table.setWidthPercentage(100);
-    table.setSpacingBefore(10);
-    table.setSpacingAfter(0);
-
-    table
-        .addCell(getDetailCell("Datum", Element.ALIGN_CENTER, Color.LIGHT_GRAY));
-    table
-        .addCell(getDetailCell("Name", Element.ALIGN_CENTER, Color.LIGHT_GRAY));
-    table.addCell(getDetailCell("Zahlungsgrund", Element.ALIGN_CENTER,
-        Color.LIGHT_GRAY));
-    table.addCell(getDetailCell("Zahlungsgrund2", Element.ALIGN_CENTER,
-        Color.LIGHT_GRAY));
-    table.addCell(getDetailCell("Betrag", Element.ALIGN_CENTER,
-        Color.LIGHT_GRAY));
-    table.setHeaderRows(1);
-    return table;
-  }
-
-  private void createTableContent(Document rpt, DBIterator list,
-      PdfPTable table, Date dVon, Date dBis) throws RemoteException,
+  private void createTableContent(Reporter reporter, DBIterator list,
+      Konto konto, Date dVon, Date dBis) throws RemoteException,
       DocumentException
   {
     Buchungsart ba = null;
@@ -251,12 +159,16 @@ public class BuchungAuswertungPDF
           FontFactory.HELVETICA_BOLD, 10));
     }
 
-    rpt.add(pBuchungsart);
+    reporter.add(pBuchungsart);
     DBIterator listb = Einstellungen.getDBService().createList(Buchung.class);
     listb.addFilter("datum >= ?", new Object[] { new java.sql.Date(dVon
         .getTime()) });
     listb.addFilter("datum <= ?", new Object[] { new java.sql.Date(dBis
         .getTime()) });
+    if (konto != null)
+    {
+      listb.addFilter("konto = ?", new Object[] { konto.getID() });
+    }
     if (list != null)
     {
       listb.addFilter("buchungsart = ?",
@@ -268,34 +180,39 @@ public class BuchungAuswertungPDF
     }
     listb.setOrder("ORDER BY datum");
     double buchungsartSumme = 0;
+    createTableHeader(reporter);
     while (listb.hasNext())
     {
       Buchung b = (Buchung) listb.next();
-      table.addCell(getDetailCell(
-          Einstellungen.DATEFORMAT.format(b.getDatum()), Element.ALIGN_LEFT));
-      table.addCell(getDetailCell(b.getName(), Element.ALIGN_LEFT));
-      table.addCell(getDetailCell(b.getZweck(), Element.ALIGN_LEFT));
-      table.addCell(getDetailCell(b.getZweck2(), Element.ALIGN_LEFT));
-      table.addCell(getDetailCell(Einstellungen.DECIMALFORMAT.format(b
-          .getBetrag()), Element.ALIGN_RIGHT));
+      reporter.addColumn(reporter.getDetailCell(Einstellungen.DATEFORMAT
+          .format(b.getDatum()), Element.ALIGN_LEFT));
+      reporter.addColumn(reporter
+          .getDetailCell(b.getName(), Element.ALIGN_LEFT));
+      reporter.addColumn(reporter.getDetailCell(b.getZweck(),
+          Element.ALIGN_LEFT));
+      reporter.addColumn(reporter.getDetailCell(b.getZweck2(),
+          Element.ALIGN_LEFT));
+      reporter.addColumn(reporter.getDetailCell(Einstellungen.DECIMALFORMAT
+          .format(b.getBetrag()), Element.ALIGN_RIGHT));
       buchungsartSumme += b.getBetrag();
     }
-    table.addCell(getDetailCell("", Element.ALIGN_LEFT));
+    reporter.addColumn(reporter.getDetailCell("", Element.ALIGN_LEFT));
     if (list != null)
     {
-      table.addCell(getDetailCell("Summe " + ba.getBezeichnung(),
+      reporter.addColumn(reporter.getDetailCell("Summe " + ba.getBezeichnung(),
           Element.ALIGN_LEFT));
     }
     else
     {
-      table.addCell(getDetailCell("Summe ohne Zuordnung", Element.ALIGN_LEFT));
+      reporter.addColumn(reporter.getDetailCell("Summe ohne Zuordnung",
+          Element.ALIGN_LEFT));
     }
     summe += buchungsartSumme;
-    table.addCell(getDetailCell("", Element.ALIGN_LEFT));
-    table.addCell(getDetailCell("", Element.ALIGN_LEFT));
-    table.addCell(getDetailCell(Einstellungen.DECIMALFORMAT
+    reporter.addColumn(reporter.getDetailCell("", Element.ALIGN_LEFT));
+    reporter.addColumn(reporter.getDetailCell("", Element.ALIGN_LEFT));
+    reporter.addColumn(reporter.getDetailCell(Einstellungen.DECIMALFORMAT
         .format(buchungsartSumme), Element.ALIGN_RIGHT));
-    rpt.add(table);
+    reporter.closeTable();
   }
 
 }
