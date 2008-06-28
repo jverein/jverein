9c910374b1a92c3997c32c6cda2a0bba80237cca
jost <>
2008-06-28 17:08:12 +0000

    refactoring

--- src/de/jost_net/JVerein/util/Geschaeftsjahr.java
+++ src/de/jost_net/JVerein/util/Geschaeftsjahr.java
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log$
+ * Revision 1.2  2008/05/26 18:59:31  jost
+ * neue Methode
+ *
  * Revision 1.1  2008/05/25 19:37:40  jost
  * Neu
  *
@@ -29,13 +32,34 @@ public class Geschaeftsjahr
 
   private Date endeGeschaeftsjahr;
 
-  public Geschaeftsjahr(String tagmonat, int jahr) throws ParseException
+  public Geschaeftsjahr(int jahr) throws ParseException
   {
     beginnGeschaeftsjahr = Datum.toDate(Einstellungen.getBeginnGeschaeftsjahr()
         + jahr);
     Calendar cal = Calendar.getInstance();
     cal.setTime(beginnGeschaeftsjahr);
     beginnGeschaeftsjahrjahr = cal.get(Calendar.YEAR);
+    cal.add(Calendar.YEAR, 1);
+    cal.add(Calendar.DAY_OF_MONTH, -1);
+    endeGeschaeftsjahr = cal.getTime();
+  }
+
+  /**
+   * Geschäftsjahr zu einem vorgegebenen Datum ermitteln
+   */
+  public Geschaeftsjahr(Date datum) throws ParseException
+  {
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(datum);
+    beginnGeschaeftsjahr = Datum.toDate(Einstellungen.getBeginnGeschaeftsjahr()
+        + cal.get(Calendar.YEAR));
+    if (datum.before(beginnGeschaeftsjahr))
+    {
+      cal.add(Calendar.YEAR, -1);
+      beginnGeschaeftsjahr = Datum.toDate(Einstellungen
+          .getBeginnGeschaeftsjahr()
+          + cal.get(Calendar.YEAR));
+    }
     cal.add(Calendar.YEAR, 1);
     cal.add(Calendar.DAY_OF_MONTH, -1);
     endeGeschaeftsjahr = cal.getTime();
