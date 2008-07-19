28c36a71b789bed0cd5363d0bf4563dde580c7e9
jost <>
2008-07-19 19:24:58 +0000

    Neu: Spendenbescheinigung

---
+++ src/de/jost_net/JVerein/gui/action/SpendenbescheinigungDeleteAction.java
@@ -0,0 +1,77 @@
+/**********************************************************************
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * Copyright (c) by Heiner Jostkleigrewe
+ * All rights reserved
+ * heiner@jverein.de
+ * www.jverein.de
+ * $Log$
+ **********************************************************************/
+package de.jost_net.JVerein.gui.action;
+
+import java.rmi.RemoteException;
+
+import de.jost_net.JVerein.rmi.Formular;
+import de.jost_net.JVerein.rmi.Spendenbescheinigung;
+import de.willuhn.jameica.gui.Action;
+import de.willuhn.jameica.gui.GUI;
+import de.willuhn.jameica.gui.dialogs.YesNoDialog;
+import de.willuhn.jameica.gui.parts.TablePart;
+import de.willuhn.logging.Logger;
+import de.willuhn.util.ApplicationException;
+
+/**
+ * Loeschen einer Spendenbescheinigung
+ */
+public class SpendenbescheinigungDeleteAction implements Action
+{
+  public void handleAction(Object context) throws ApplicationException
+  {
+    if (context instanceof TablePart)
+    {
+      TablePart tp = (TablePart) context;
+      context = tp.getSelection();
+    }
+    if (context == null || !(context instanceof Spendenbescheinigung))
+    {
+      throw new ApplicationException("Keine Spendenbescheinigung ausgewählt");
+    }
+    try
+    {
+      Spendenbescheinigung spb = (Spendenbescheinigung) context;
+      if (spb.isNewObject())
+      {
+        return;
+      }
+
+      YesNoDialog d = new YesNoDialog(YesNoDialog.POSITION_CENTER);
+      d.setTitle("Spendenbescheinigung löschen");
+      d.setText("Wollen Sie die Spendenbescheinigung wirklich löschen?");
+
+      try
+      {
+        Boolean choice = (Boolean) d.open();
+        if (!choice.booleanValue())
+        {
+          return;
+        }
+      }
+      catch (Exception e)
+      {
+        Logger.error("Fehler beim Löschen der Spendenbescheinigung", e);
+        return;
+      }
+      spb.delete();
+      GUI.getStatusBar().setSuccessText("Spendenbescheinigung gelöscht.");
+    }
+    catch (RemoteException e)
+    {
+      String fehler = "Fehler beim Löschen der Spendenbescheinigung";
+      GUI.getStatusBar().setErrorText(fehler);
+      Logger.error(fehler, e);
+    }
+  }
+}
