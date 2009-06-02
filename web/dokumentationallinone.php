7d3b7e232184ad411067c81098eb6123c22daf93
jost <>
2009-06-02 17:13:13 +0000

    *** empty log message ***

---
+++ web/dokumentationallinone.php
@@ -0,0 +1,38 @@
+	<? include ("jameica_startseite.php"); ?>
+	<h2>Administration</h2>
+	<? include ("administration_stammdaten.php"); ?>
+	<? include ("administration_beitragsgruppen.php"); ?>
+	<? include ("administration_einstellungen.php"); ?>
+	<? include ("administration_felddefinitionen.php"); ?>
+	<? include ("administration_formulare.php"); ?>
+	<? include ("administration_erweitert.php"); ?>
+	<? include ("administration_import.php"); ?>
+	<? include ("mitglied.php"); ?>
+	<? include ("kursteilnehmer.php"); ?>
+	<? include ("abrechnung.php"); ?>
+	<? include ("zusatzbetraege.php"); ?>
+	<? include ("manuelle_zahlungseingaenge.php"); ?>
+	<? include ("wiedervorlage.php"); ?>
+	<? include ("lehrgaenge.php"); ?>
+	<? include ("spendenbescheinigung.php"); ?>
+	<? include ("rechnungen.php"); ?>
+	<? include ("dokumentationauswertung.php"); ?>
+	<? include ("dokumentationauswertungmitglieder.php"); ?>
+	<? include ("dokumentationauswertungjubilaeen.php"); ?>
+	<? include ("dokumentationauswertungkursteilnehmer.php"); ?>
+	<? include ("dokumentationauswertungstatistik.php"); ?>
+	<? include ("dokumentationauswertungadressbuchexport.php"); ?>
+	<? include ("dokumentationbuchfuehrung.php"); ?>
+	<? include ("dokumentationbuchfuehrungkonten.php"); ?>
+	<? include ("dokumentationbuchfuehrunganfangsbestaende.php"); ?>
+	<? include ("dokumentationbuchfuehrungbuchungenaushibiscusuebernehmen.php"); ?>
+	<? include ("dokumentationbuchfuehrungbuchungen.php"); ?>
+	<? include ("dokumentationbuchfuehrungjahressaldo.php"); ?>
+	<? include ("dokumentationbuchfuehrungjahresabschluss.php"); ?>
+	<? include ("dokumentationbeitragsmodelle.php"); ?>
+	<? include ("dokumentationmigrationinsh2format.php"); ?>
+	<? include ("jameica_portable.php"); ?>
+
+    <!-- 
+    $Log$
+    -->
