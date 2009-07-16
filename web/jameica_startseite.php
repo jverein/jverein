f8cf784556db0d6989123d02c0da6511a058b1d3
jost <>
2009-07-16 16:45:23 +0000

    Neu: Aktuelle Geburtstage

--- web/jameica_startseite.php
+++ web/jameica_startseite.php
@@ -1,14 +1,32 @@
 <? include ("frame.inc"); ?>
     <h1>Jameica Startseite</h1>
     <p>
-    In die Jameica-Startseite kann eine Liste der Wiedervorlagetermine eingeblendet werden. Dazu wird 
-    die Startseite von Jameica geöffnet. Unten rechts befindet sich der Knopf Startseite anpassen. Ein 
-    Klick darauf öffnet das Konfigurationsfenster. Mit einem Rechtsklick auf JVerein: Wiedervorlage kann 
-    die Anzeige aktiviert oder deaktiviert werden. 
-    </p>
+    In die Jameica-Startseite können 
+    <ul>
+    	<li>die aktuellen Geburtstage</li>
+    	<li>die Wiedervorlagetermine</li>
+    </ul>
+    eingeblendet werden.
+    
+    Auf der Jameica-Seite können die Einstellungen unter 'Startseite anpassen' 
+    <img src='images/JameicaStartseite.jpg' class='screenshot'>
+    
+    
+    <h2>Aktuelle Geburtstage</h2>
+    <img src='images/AktuelleGeburtstage.jpg' class='screenshot'>
+    
+    <p>Unter Administration>Einstellungen kann der Zeitraum der Geburtstagsliste (Tage
+    vor dem aktuellen und nach dem aktuellen Datum) eingestellt werden.</p>
+
+    <h2>Wiedervorlage</h2>
+    <img src='images/WiedervorlageBox.jpg' class='screenshot'>
+    
     
     <!-- 
     $Log$
+    Revision 1.1  2009/05/08 14:46:22  jost
+    shtml - php
+
     Revision 1.2  2009/05/03 15:33:30  jost
     Neue Homepage
 
