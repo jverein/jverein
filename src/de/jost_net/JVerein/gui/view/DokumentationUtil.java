6987873c90c93a023bce0f0d3493ac9cd3dd3f2a
jost <>
2009-08-09 15:28:14 +0000

    Umstellung auf neue Web-Site

--- src/de/jost_net/JVerein/gui/view/DokumentationUtil.java
+++ src/de/jost_net/JVerein/gui/view/DokumentationUtil.java
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log$
+ * Revision 1.11  2009/04/13 11:40:14  jost
+ * Neu: Lehrgänge
+ *
  * Revision 1.10  2009/03/26 21:02:24  jost
  * Neu: Adressbuchexport
  *
@@ -50,70 +53,73 @@ public class DokumentationUtil
 
   }
 
-  private static final String PRE = "http://www.jverein.de/index.php5?title=";
+  private static final String PRE = "http://www.jverein.de/neu/";
 
-  public static final String ABBUCHUNG = PRE + "Abbuchung";
+  public static final String ADRESSBUCHEXPORT = PRE
+      + "dokumentationauswertungadressbuchexport.php";
 
-  public static final String ADRESSBUCHEXPORT = PRE + "Adressbuchexport";
+  public static final String ABRECHNUNG = PRE + "abrechnung.php";
 
-  public static final String ABRECHNUNG = PRE + "Abrechnung";
-
-  public static final String RECHNUNG = PRE + "Rechnung";
+  public static final String RECHNUNG = PRE + "rechnungen.php";
 
   public static final String AUSWERTUNGKURSTEILNEHMER = PRE
-      + "Auswertung#Auswertung_der_Kursteilnehmer";
+      + "dokumentationauswertungkursteilnehmer.php";
 
   public static final String AUSWERTUNGMITGLIEDER = PRE
-      + "Auswertung#Auswertung_der_Mitglieder";
+      + "dokumentationauswertungmitglieder.php";
 
   public static final String BEITRAGSGRUPPEN = PRE
-      + "Administration#Beitragsgruppen";
+      + "administration_beitragsgruppen.php";
 
   public static final String FELDDEFINITIONEN = PRE
-      + "Administration#Felddefinitionen";
+      + "administration_felddefinitionen.php";
 
-  public static final String FORMULARE = PRE + "Administration#Formulare";
+  public static final String FORMULARE = PRE + "administration_formulare.php";
 
   public static final String EINSTELLUNGEN = PRE
-      + "Administration#Einstellungen";
+      + "administration_einstellungen.php";
 
-  public static final String IMPORT = PRE + "Administration#Import";
+  public static final String IMPORT = PRE + "administration_import.php";
 
-  public static final String JUBILAEEN = PRE + "Auswertung#Jubil.C3.A4en";
+  public static final String JUBILAEEN = PRE
+      + "dokumentationauswertungjubilaeen.php";
 
-  public static final String LEHRGANG= PRE + "Lehrgang";
+  public static final String LEHRGANG = PRE + "lehrgaenge.php";
 
-  public static final String MITGLIED = PRE + "Mitglieder";
+  public static final String MITGLIED = PRE + "mitglied.php";
 
-  public static final String KURSTEILNEHMER = PRE + "Kursteilnehmer";
+  public static final String KURSTEILNEHMER = PRE + "kursteilnehmer.php";
 
   public static final String MANUELLEZAHLUNGSEINGAENGE = PRE
-      + "Manuelle_Zahlungseing%C3%A4nge";
+      + "manuelle_zahlungseingaenge.php";
 
-  public static final String STAMMDATEN = PRE
-      + "Administration#Stammdaten_des_Vereins";
+  public static final String STAMMDATEN = PRE + "administration_stammdaten.php";
 
-  public static final String STATISTIKMITGLIEDER = PRE + "Auswertung#Statistik";
+  public static final String STATISTIKMITGLIEDER = PRE
+      + "dokumentationauswertungstatistik.php";
 
-  public static final String WIEDERVORLAGE = PRE + "Wiedervorlage";
+  public static final String WIEDERVORLAGE = PRE + "wiedervorlage.php";
 
-  public static final String ZUSATZABBUCHUNGEN = PRE + "Zusatzabbuchungen";
+  public static final String ZUSATZABBUCHUNGEN = PRE + "zusatzbetraege.php";
 
-  public static final String KONTEN = PRE + "Konten";
+  public static final String KONTEN = PRE
+      + "dokumentationbuchfuehrungkonten.php";
 
-  public static final String ANFANGSBESTAENDE = PRE + "Anfangsbestände";
+  public static final String ANFANGSBESTAENDE = PRE
+      + "dokumentationbuchfuehrunganfangsbestaende.php";
 
   public static final String BUCHUNGENAUSHIBISCUS = PRE
       + "Buchungen_aus_Hibiscus_übernehmen";
 
-  public static final String BUCHUNGEN = PRE + "Buchungen";
+  public static final String BUCHUNGEN = PRE
+      + "dokumentationbuchfuehrungbuchungenaushibiscusuebernehmen.php";
 
-  public static final String JAHRESSALDO = PRE + "Jahressaldo";
+  public static final String JAHRESSALDO = PRE
+      + "dokumentationbuchfuehrungjahressaldo.php";
 
-  public static final String JAHRESABSCHLUSS = PRE + "Jahresabschluss";
+  public static final String JAHRESABSCHLUSS = PRE
+      + "dokumentationbuchfuehrungjahresabschluss.php";
 
   public static final String SPENDENBESCHEINIGUNG = PRE
-      + "Spendenbescheinigung";
-
-  public static final String REPORTS = PRE + "Reports";
+      + "spendenbescheinigung.php";
 }
