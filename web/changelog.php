c4d7f31ac8c823e380240fd37795a0fe4e45eaf0
jost <>
2009-09-15 05:58:51 +0000

    *** empty log message ***

--- web/changelog.php
+++ web/changelog.php
@@ -7,7 +7,7 @@
     			<li>Vermeidung NullPointerException beim Jahressaldo.</li>
     			<li>Neu: Buchungsjournal, Feature Request <a href='https://developer.berlios.de/bugs/?func=detailbug&bug_id=16103&group_id=7335'>#16103</a></li>
     			<li>Neu: Überprüfung auf Updates.</li>
-    			<li>Auswertung Mitglieder: Mitglieder mit Austrittsdatum in der Zukunft werden mit ausgewertet. Bug <a href='https://developer.berlios.de/bugs/?func=detailbug&bug_id=16103&group_id=7335'>#16103</a></li>
+    			<li>Auswertung Mitglieder: Mitglieder mit Austrittsdatum in der Zukunft werden mit ausgewertet. Bug <a href='https://developer.berlios.de/bugs/?func=detailbug&bug_id=16223&group_id=7335'>#16223</a></li>
     		</ul>
     	<li>Version 1.2 vom 23.8.2009</li>
     		<ul>
@@ -436,6 +436,9 @@
 </ul>
     <!-- 
     $Log$
+    Revision 1.18  2009/09/15 05:55:22  jost
+    *** empty log message ***
+
     Revision 1.17  2009/09/14 19:16:16  jost
     *** empty log message ***
 
