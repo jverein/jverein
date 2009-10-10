1875295909fd09d528964dd0d427314de04a7369
jost <>
2009-10-10 16:04:03 +0000

    *** empty log message ***

---
+++ web/buchfuehrungzusammenhaenge.php
@@ -0,0 +1,201 @@
+<? include ("frame.inc"); ?>
+    <h1>Unterstützung von JVerein bei den Aufgaben eines Kassenwarts und den Kassenprüfern</h1>
+    
+    <h2> Zweck</h2>
+	<p>Da die Aufgaben des Kassenwarts und der Kassenprüfer in einer Vereinssatzung oft nur sehr 
+		grob umschrieben werden, es aber dennoch einige Regeln und Vorschriften zu beachten gibt, 
+		besteht unter Umständen eine Unsicherheit bei der korrekten Ausführung dieser Tätigkeit.</p> 
+	<p>	Will man dann auch noch entscheiden, ob der Wechsel (oder Neueinsatz) einer Vereinssoftware 
+		wie JVerein sinnvoll und machbar ist, wird die Unsicherheit noch größer.</p>
+	<p> Erschwerend kommt 
+		manchmal hinzu, dass oftmals die Vorstellung eines Buchhalters und eines "Nichtbuchhalters"
+		in puncto Kassenführung sehr unterschiedlich sind, und eine doppelte Buchführung mit einem 
+		angepassten "Industriekontenrahmen" für einen "Nichtbuchhalter" nur schwer überblickbar ist.</p>
+	<p>	Deshalb hier eine kurze Beschreibung der Aufgaben eines Kassenwarts und der Kassenprüfer 
+		und wie diese Tätigkeiten durch JVerein unterstützt werden können.</p>
+	<p>	<u>Hinweis:</u> Der Autor war selbst über 10 Jahre Kassenwart in einem Musikverein und ist 
+		jetzt über 2 Jahre Kassenwart in einem Sportverein und hat somit schon einige Erfahrung auf 
+		diesem Gebiet sammeln können.</p>
+		
+	<h2>Grundlagen</h2>
+	<h3>Aufgaben des Kassenwarts</h3>
+	
+	<p>Oftmals wird in der Vereinssatzung nur sehr knapp die Aufgaben des Kassenwarts beschrieben, 
+		z.B. "Die Aufgaben des Kassenwarts erstrecken sich auf die Verwaltung des Vereinsvermögens. 
+		Über Einnahmen und Ausgaben ist Buch zu führen". Diese Beschreibung ist zwar richtig, jedoch 
+		wird damit nicht ansatzweise beschrieben, nach welchen Gesetzen und Verordnungen das zu 
+		geschehen hat.</p> 
+	<p>Sicher ist damit nur, dass man "Buch zu führen hat", und das bedeutet dass man chronologisch, 
+		systematisch, lückenlos und ordnungsgemäß alle in Zahlenwerten festgehaltenen, wirtschaftlich 
+		bedeutsamen Vorgänge (Geschäftsvorfälle) verzeichnen muss.</p>
+	<p>Wichtig zu wissen ist, dass sich diese Tätigkeit an das HGB anlehnt, und dabei sind vor allem 
+		der § 243 (Aufstellungsgrundsatz) und der § 246 (Vollständigkeit. Verrechnungsverbot) zu nennen. </p>
+
+	<p>Mit diesem Wissen ergeben sich folgende Aufgaben und Grundsätze, die eingehalten werden müssen:
+	<ol>
+		<li>Im allgemeinen kann die Verwaltung der Kasse mit einer einfachen <b>"Einnahmen-Ausgaben-Rechnung"</b>
+			erfolgen, und es muss keine doppelte Buchführung erstellt werden.</li>
+		<li>Der Jahresabschluss ist nach den <b>Grundsätzen ordentlicher Buchführung (GOB)</b> aufzustellen.
+			<ul>
+				<li>Klarheit und Übersichtlichkeit</li>
+				<li>Vollständigkeit und Richtigkeit</li>
+			</ul>
+		</li>
+		<li>Es gilt der Grundsatz der "Klarheit und Übersichtlichkeit" (§ 243 HGB), das bedeutet die 
+			<b>Nachvollziehbarkeit</b> jeder Buchung aber auch des Jahresabschlusses.</li>
+		<li>Es gilt der <b>"Grundsatz der Vollständigkeit"</b> (§ 246 HGB), dies bedeutet dass alle Einnahmen, 
+			Ausgaben, Belege, Buchungen vorhanden sein müssen und dass keine Einnahmen mit Ausnahmen 
+			verrechnet werden dürfen.</li>
+		<li>Zu guter Letzt hat der Kassenwart eine <b>Auskunfts- und Informationspflicht</b> gegenüber dem 
+			Vorstand (und auch dem Finanzamt). Dies beinhaltet z.B. Auskunft über die finanzielle 
+			Situation aber auch Detailauskünfte z.B. für eine Entscheidung in der Vorstandsschaft. </li>
+
+	</ol>	
+	<h3>Aufgaben der Kassenprüfer</h3>
+	<p>Auch die Aufgabe der Kassenprüfer wird in der Vereinssatzung oft nur sehr grob beschrieben, z.B. 
+		mit folgendem Wortlaut: "Die Kassenprüfer sind verpflichtet alle Unterlagen des Kassenwarts zu 
+		überprüfen".</p>
+	<p>Da jedoch der Kassenwart einige Regeln zu beachten hat (siehe vorheriges Kapitel) ist es natürlich 
+		sinnvoll, dass diese Punkte von den Kassenprüfern auch überprüft werden. Zusätzlich sollte aber 
+		auch die sachliche Richtigkeit überprüft werden, wie z.B. die Gruppierung der Einnahmen bzw. 
+		Ausgaben (siehe Kapitel "Buchungsklassen"). Damit ergeben sich zumindest folgende wichtigen 
+		Aufgaben:
+		<ol>
+			<li>Überprüfung des Jahresabschlusses auf Richtigkeit, z.B. stimmen die Anfangs- und Endwerte 
+				mit den Salden in den Kontoauszügen überein?</li>
+			<li>Überprüfung der Nachvollziehbarkeit und Übersichtlichkeit, z.B. kann jede Buchung 
+				nachvollzogen werden?</li>
+			<li>Überprüfung auf Vollständigkeit, z.B. sind alle Belege vorhanden, alle Einnahmen und 
+				Ausgaben verbucht und gehören diese auch in das entsprechende Geschäftsjahr?</li>
+			<li>Überprüfung auf sachliche Richtigkeit, z.B. sind die Buchungen richtig den einzelnen 
+				"Gruppen" zugeordnet, z.B. nur Spenden sind in der Gruppe "Spende" verbucht und keine 
+				Einnahmen von einer Tombola.</li>
+		</ol>
+		
+	<h2>Listen von JVerein für die Buchführung</h2>
+    <p>In diesem Kapitel werden die einzelnen Listen der Buchführung von JVerein beschrieben und wie 
+    	diese miteinander den Kassenwart und die Kassenprüfer bei Ihrer Arbeit unterstützen.</p>
+	<p>Normalerweise werden alle notwendigen Listen erstellt, bevor der "Jahresabschluss" 
+		(Navigation: Buchführung => Jahresabschluss) gemacht wird. Durch den Jahresabschluss wird das 
+		Geschäftsjahr endgültig abgeschlossen, dies bedeutet dass keine Änderungen (z.B. bei Buchungen) 
+		in dem abgeschlossenen Geschäftsjahr mehr möglich sind.</p>
+	<p>Für die Kassenprüfung werden meist nur die Liste "Jahressaldo" und die Liste "Buchungsjournal" 
+		benötigt, da mit diesen beiden Listen alle Transaktionen und deren sachlichen Zuordnung nachvollzogen 
+		werden kann (eventuell sollte die Liste "Buchungsklassen" noch bereitgelegt werden, da diese für 
+		eine eventuelle Meldung fürs Finanzamt wichtig ist und außerdem damit der Kassenbericht relativ 
+		einfach angefertigt werden kann).</p>
+	<p>Entsprechend werden die restlichen Listen nur für besondere Fragestellungen benötigt (z.B. als 
+		Grundlage für eine Entscheidung in der Vorstandsschaft).</p>
+		
+	<h3>Jahressaldo</h3>
+	<p>Die Liste "Jahressaldo" (Navigation: Buchführung => Jahressaldo) stellt die Verbindung zu den 
+		realen Konten und zum Vorjahr her, sprich es wird für jedes Konto der Anfangsbestand, Einnahmen, 
+		Ausgaben, Umbuchungen und Endbestand aufgeführt.</p>
+	    <img src='images/listejahressaldo.png' class='screenshot'>
+	    
+	<p>	Diese Liste stellt entsprechend auch die Basis für die Kassenprüfung dar. Die Anfangsbestände 
+		finden sich auf der "Jahressaldo-Liste" des Vorjahres wieder und können damit verglichen werden. 
+	    Die Endbestände der Bankkonten können mit den entsprechenden Kontoauszügen verglichen werden, 
+	    der Bestand der Kasse wird gezählt.</p>
+	<p> Somit ist der Jahresendbestand definiert und im nächsten Schritt können die Einnahmen und 
+	   	Ausgaben kontrolliert werden. Dazu dient das Buchungsjournal (siehe nächstes Kapitel).</p>
+	<p><u>Hinweis:</u> Da auf der "Jahressaldo-Liste" die wichtigsten Zahlenwerte stehen und diese 
+		zur nächsten Kassenprüfung im Folgejahr wieder benötigt werden, ist es sinnvoll, am Ende der 
+		Kassenprüfung exakt auf dieser Liste die Kassenprüfer mit Unterschrift bestätigen zu lassen, 
+		dass die Kasse geprüft wurde und alles in Ordnung ist. Damit ist gewährleistet, dass die 
+		Kassenprüfer im Folgejahr auch die richtigen Anfangsbestände verwenden!</p>
+			
+	<h3>Buchungsjournal</h3>
+	<p> Das "Buchungsjournal" (Navigation: Buchführung => Buchungen => PDF Buchungsjournal)  ist die
+		zentrale Liste mit der die komplette Kasse überprüft werden kann, da die gesamte Arbeit damit 
+		abgebildet wird.</p>
+	    <img src='images/listebuchungsjournal.png' class='screenshot'>
+	<p> Um die Vollständigkeit zu gewährleisten sind nur folgende Selektionen der Buchungen erlaubt, 
+		die entsprechend in der Überschrift der Liste erscheinen:
+	<ul>
+		<li>Zeitraum: Für die Kassenprüfung sollte natürlich dann das komplette Geschäftsjahr (meist 
+			das Kalenderjahr) selektiert werden.</li>>
+		<li>Einzelnes Konto oder alle Konten: Diese Selektion hängt von der Arbeitsweise ab, entweder 
+			man hat nur ein Ordner mit Belegen (dann sollte entsprechend auch alle Konten selektiert 
+			werden) oder man hat einen Belegordner pro Konto, dann sollten entsprechend für jedes Konto 
+			ein eigenes Buchungsjournal selektiert werden.</li>
+	</ul>
+	<p> Die Sortierung erfolgt grundsätzlich nach Datum, da die Belege im Belegordner normalerweise 
+		auch nach Datum sortiert werden (Hinweis: Die Buchungsnummer erlaubt meist keine sinnvolle 
+		Sortierung, da oftmals die Belege erst verspätet beim Kassenwart abgeliefert werden und somit 
+		nachträglich (allerdings mit dem richtigen Datum) gebucht werden.</p>
+	<p> Für die Nachvollziehbarkeit und sachlicher Zuordnung (die die Kassenprüfer normalerweise auch 
+		überprüfen sollten) sind folgende Felder im Buchungsjournal enthalten:
+		<ul>
+			<li>Buchungsnummer (Nr): Dient zur Zuordnung der Belege zur Buchung indem jeder Beleg mit 
+				der entsprechenden Buchungsnummer beschriftet wird (= Belegnummer).<br>
+				<u>Hinweis:</u> Gerade die Kassenprüfer sind dankbar, wenn sie anhand einer eindeutigen 
+				Nummer den relevanten Beleg im Ordner schnell finden können.</li>
+			<li>Datum: Ist das Sortierkriterium</li>
+			<li>Konto und Auszug: Damit kann schnell die Transaktion auf dem entsprechenden Kontoauszug 
+				gefunden werden</li>
+			<li>Name und Zahlungsgrund: Beschreiben inhaltlich die Buchung</li>
+			<li>Buchungsart: Stellt die sachliche Zuordnung der Buchung zu einer übergeordneten Gruppe
+				dar (z.B. dürfen Einnahmen aus einer Tombola nicht plötzlich als Spenden gebucht werden). 
+				Dies ist auch für die Nachvollziehbarkeit fürs Finanzamt wichtig (siehe Kapitel 
+				"Buchungsklassen").</li>
+			<li>Betrag: Der Betrag wird nach Ausgaben und Einnahmen unterschieden, um die Salden ermitteln 
+				zu können.</li>
+	</p>
+	<p>Am Ende der Liste stehen die Summen für Einnahmen, Ausgaben und Umbuchungen. Diese Summen  müssen 
+		exakt mit den Werten für Einnahmen, Ausgaben und Umbuchungen im "Jahressaldo" übereinstimmen. 
+		Somit wird die Vollständigkeit der Buchungen und damit auch der Belege gewährleistet, da alle 
+		Transaktionen im Geschäftsjahr nachgewiesen werden.</p> 
+
+	<h3>Buchungsartenliste (Summen)</h3>
+	<p> Die "Buchungsartenliste-Summen" (Navigation: Buchführung => Buchungen => PDF Summen) dient dazu 
+		einen Gesamtüberblick über die finanzielle Situation zu erhalten,  um z.B. einen Kassenbericht 
+		anzufertigen.</p>
+	    <img src='images/listebuchungsartensummen.png' class='screenshot'>
+	<p>	Entsprechend wird auch am Ende dieser Liste die Summen getrennt nach Einnahmen, Ausgaben und 
+		Umbuchungen aufgeführt.</p>
+	
+	<h3> Buchungsartenliste (Einzelbuchungen)</h3>
+	<p> Die "Buchungsartenliste-Einzelbuchungen" (Navigation: Buchführung => Buchungen => PDF Einzelbuchungen) 
+		dient für die Klärung einer Detailfrage, z.B. "Wer hat dieses Jahr schon alles gespendet?" oder "Welche 
+		Sportgeräte wurden dieses Jahr schon gekauft?").</p>	
+	    <img src='images/listebuchungen.png' class='screenshot'>
+	<p> Deshalb werden entsprechend der Selektion nur die relevanten Buchungen gruppiert nach den Kostenarten 
+		und mit den gesamten Informationen (Name, Zahlungsgrund, Zahlungsgrund2) dargestellt.</p>
+	
+	<h3>Buchungsklassenliste</h3>
+	<p> Die Buchungsklassen dienen dazu die einzelnen Buchungen entsprechend den steuerlichen Vorgaben zu 
+		gruppieren. Diese Klassen können in JVerein unter "Administration => Buchungsklasse" angelegt und 
+		gepflegt werden.</p>
+	<p> Für Vereine würde sich folgende Aufteilung anbieten, da diese Gruppierung vom Finanzamt angefragt 
+		werden kann, da z.B. für die Steuerbefreiung bestimmte Grenzwerte in diesen Gruppen nicht 
+		überschritten werden dürfen:
+		<ul>
+			<li>Ideeller Bereich</li>
+			<li>Vermögensverwaltung</li>
+			<li>Zweckbetrieb</li>
+			<li>Wirtschaftlicher Geschäftsbetrieb</li>
+		</ul>
+		</p>
+	<p> Unter Administration => Buchungsart werden die Buchungsarten angelegt und gepflegt. Dort können die 
+		Buchungsarten auch den einzelnen Buchungsklassen zugeordnet werden. Wichtig dabei ist natürlich, 
+		die Buchungsarten so zu definieren, dass diese auch eindeutig zu einer Buchungsklasse zugeordnet 
+		werden können.</p>
+	<p> Die "Buchungsklassenliste" (Navigation: Buchführung => Buchungsklassen) zeigt nun das Ergebnis 
+		dieser Aufteilung für ein Geschäftsjahr an.</p>
+	<p> Die Vollständigkeit der Liste wird dadurch gewährleistet dass zum einen die Gesamtsummen für 
+		Einnahmen und Ausgaben ("Gesamtsaldo" in der zweitletzten Zeile) exakt den Gesamtsummen im 
+		Jahressaldo entsprechen müssen und zweitens eventuelle Buchungen die keiner Kostenart bzw. 
+		Kostenarten die keiner Kostenklasse zugeordnet sind (was nicht sein sollte) auf dieser Liste 
+		gesondert aufgeführt werden.</p>
+	<p> Die Nachvollziehbarkeit wird dadurch gewährleistet, dass jede Buchung genau einer Kostenart 
+		zugeordnet ist und dies im Buchungsjournal aufgeführt wird (und von den Kassenprüfern hoffentlich 
+		kontrolliert wurde). Somit kann für jede Zeile in der "Buchungsklassenliste" nachvollzogen werden, 
+		welche Buchungen im einzelnen zu diesem Wert geführt haben.</p>
+	
+   <!-- 
+    $Log$
+    -->
+
+<? include ("footer.inc"); ?>
+
