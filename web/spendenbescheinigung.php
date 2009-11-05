48793563952e167111c98efdb48503a4d9f2352d
jost <>
2009-11-05 20:08:35 +0000

    Hinweis auf Rechtsklick in der Mitgliedersuche aufgenommen.

--- web/spendenbescheinigung.php
+++ web/spendenbescheinigung.php
@@ -8,12 +8,18 @@
 		Mit einem Doppelklick auf eine Spendenbescheinigung öffnet sich das Bearbeitungsfenster (siehe unten). 
 		Mit einem Rechtsklick öffnet sich ein Kontextmenü. Damit können Spendenbescheinigungen gelöscht 
 		werden. Durch einen Klick wird die Spendenbescheinigung im PDF-Format ausgegeben.</p>
+	<p>Tipp! In der Mitgliedersuche kann man mit einem Klick auf die rechte Maustaste ein Kontextmenü öffnen.
+	   	Darin den Menüpunkt 'Spendenbescheinigung' auswählen. Dann wird das Spendenbescheinigungsformular
+	   	mit den Daten des Mitglieds gefüllt.</p>
     <img src='images/Spendenbescheinigung.jpg' class='screenshot'>
 	<p>Beispiel für eine Spendenbescheinigung:</p>
     <img src='images/Formularausgefuellt.jpg' class='screenshot'>
 
     <!-- 
     $Log$
+    Revision 1.1  2009/05/08 14:46:22  jost
+    shtml - php
+
     Revision 1.2  2009/05/03 15:33:30  jost
     Neue Homepage
 
