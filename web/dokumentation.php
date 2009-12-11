4a5b99baefa3684717c307de7da509af2dd832ac
jost <>
2009-12-11 14:04:31 +0000

    MySQL aufgenommen

--- web/dokumentation.php
+++ web/dokumentation.php
@@ -58,10 +58,14 @@
 	<li><a href="dokumentationmigrationinsh2format.php">Migration der Datenbank ins H2-Format</a></li>
 	<li><a href="jameica_portable.php">Jameica + Plugins portable</a></li>
 	<li><a href="openofficeh2.php">Mit OpenOffice-Base auf die H2-Datenbank zugreifen</a></li>
+	<li><a href="dokumentationmysql.php">JVerein mit MySQL</a></li>
 </ul>
 
     <!-- 
     $Log$
+    Revision 1.7  2009/12/02 22:32:06  jost
+    Neues Dokument: Zugriff mit OpenOffice-Base auf die H2-Datenbank
+
     Revision 1.6  2009/11/27 14:47:55  jost
     *** empty log message ***
 
