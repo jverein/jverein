6ed0870be17ed7cb7fde67866efa6b798139016c
jost <>
2010-04-11 11:28:09 +0000

    email->forum

--- web/kontakt.php
+++ web/kontakt.php
@@ -1,10 +1,13 @@
 <? include ("frame.inc"); ?>
     <h2>Kontakt</h2>
-    <p>Anregungen, Erweiterungswünsche und (möglichst keine) Fehlermeldungen bitte an
-       <a href="mailto:heiner@jverein.de">heiner@jverein.de</a> senden.</p>
+    <p>Anregungen, Erweiterungswünsche und (möglichst keine) Fehlermeldungen bitte in
+       das <a href="forum">Forum</a> posten.</p>
        
     <!-- 
     $Log$
+    Revision 1.1  2009/05/08 14:46:22  jost
+    shtml - php
+
     Revision 1.4  2009/05/03 15:33:30  jost
     Neue Homepage
 
