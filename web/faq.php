02aeb5f30e988d5877a8bafa9ee0caad49eaa38e
jost <>
2010-08-04 10:41:49 +0000

    neu: FAQ

---
+++ web/faq.php
@@ -0,0 +1,35 @@
+<? include ("frame.inc"); ?>
+    <h1>FAQ</h1>
+    <a name="neuerlaptop"></a>
+    <p>Q: Ich habe mir einen neuen Laptop zugelegt. Nun möchte ich von meinem alten Laptop alles auf den Neuen übertragen.
+          Welche Dateien bzw. Ordner muss ich mitnehmen? Muss ich das Prog. neu installieren?<br>
+       A: Es muss der Programm- und der Datenordner auf den neuen Rechner übertragen werden.</p>
+    <p>Q: Wo finde ich den Programmordner?<br>
+       A: Es gibt keinen fest definierte Platz für den Programmordner. Sofern die Jameica-Suite
+          installiert wurde, ist der Ordner unter c:\Programme\Jameica zu finden. Unter Linux 
+          wird oft unter /opt/jameica oder ~/jameica installiert.</p>
+    <p>Q: Wo finde ich den Datenordner?<br>
+       A:
+          <table>
+          <tr>
+          <th>Betriebssystem</th><th>Verzeichnis</th>
+          </tr>
+          <tr>
+          <td>Linux</td><td>/home/<username>/.jameica</td>
+          </tr>
+          <tr>
+          <td>Windows 2000/XP</td><td>C:\Dokumente und Einstellungen\<username>\.jameica</td>
+          </tr>
+          <tr>
+          <td>Windows Vista</td><td>C:\Users\<username>\.jameica oder C:\Benutzer\<username>\.jameica</td>
+          </tr>
+          <tr>
+          <td>MacOS</td><td>/Users/<username>/.jameica oder /Users/<username>/Library/jameica (falls Jameica 1.7 die erste benutzte Version war, dann letzteres, sonst das erst-genannte)</td>
+          </tr>
+          </table>    
+    <!-- 
+    $Log$
+    -->
+       
+<? include ("footer.inc"); ?>
+
