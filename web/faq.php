339f164c3f58e923a11c5b9c2f909422396b15a7
jost <>
2010-08-04 11:36:38 +0000

    redakt

--- web/faq.php
+++ web/faq.php
@@ -4,8 +4,9 @@
     <p>Q: Ich habe mir einen neuen Laptop zugelegt. Nun möchte ich von meinem alten Laptop alles auf den Neuen übertragen.
           Welche Dateien bzw. Ordner muss ich mitnehmen? Muss ich das Prog. neu installieren?<br>
        A: Es muss der Programm- und der Datenordner auf den neuen Rechner übertragen werden.</p>
+    <a name="programmordner"></a>
     <p>Q: Wo finde ich den Programmordner?<br>
-       A: Es gibt keinen fest definierte Platz für den Programmordner. Sofern die Jameica-Suite
+       A: Es gibt keinen fest definierten Platz für den Programmordner. Sofern die Jameica-Suite
           installiert wurde, ist der Ordner unter c:\Programme\Jameica zu finden. Unter Linux 
           wird oft unter /opt/jameica oder ~/jameica installiert.</p>
     <p>Q: Wo finde ich den Datenordner?<br>
@@ -15,20 +16,23 @@
           <th>Betriebssystem</th><th>Verzeichnis</th>
           </tr>
           <tr>
-          <td>Linux</td><td>/home/<username>/.jameica</td>
+          <td>Linux</td><td>/home/&lt;username&gt;/.jameica</td>
           </tr>
           <tr>
-          <td>Windows 2000/XP</td><td>C:\Dokumente und Einstellungen\<username>\.jameica</td>
+          <td>Windows 2000/XP</td><td>C:\Dokumente und Einstellungen\&lt;username&gt;\.jameica</td>
           </tr>
           <tr>
-          <td>Windows Vista</td><td>C:\Users\<username>\.jameica oder C:\Benutzer\<username>\.jameica</td>
+          <td>Windows Vista</td><td>C:\Users\&lt;username&gt;\.jameica oder C:\Benutzer\&lt;username&gt;\.jameica</td>
           </tr>
           <tr>
-          <td>MacOS</td><td>/Users/<username>/.jameica oder /Users/<username>/Library/jameica (falls Jameica 1.7 die erste benutzte Version war, dann letzteres, sonst das erst-genannte)</td>
+          <td>MacOS</td><td>/Users/&lt;username&gt;/.jameica oder /Users/&lt;username&gt;/Library/jameica (falls Jameica 1.7 die erste benutzte Version war, dann letzteres, sonst das erst-genannte)</td>
           </tr>
           </table>    
     <!-- 
     $Log$
+    Revision 1.1  2010-08-04 10:41:39  jost
+    neu: FAQ
+
     -->
        
 <? include ("footer.inc"); ?>
