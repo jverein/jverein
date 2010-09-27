e233a0e68c56ee5dde1d4f24098e42a0b5846215
jost <>
2010-09-27 20:15:16 +0000

    *** empty log message ***

---
+++ web/plan_V1_5.php
@@ -0,0 +1,15 @@
+<? include ("frame.inc"); ?>
+    <h1>Plan für die Version 1.5</h1>
+    <p>
+    <p>Für die Version 1.5 sind bis jetzt folgende Erweiterungen geplant:</p>
+   
+   	<ul>
+   		<li> Verwaltung und Abrechnung von Arbeitsdiensten</li>
+   		<li> Filterung der Mitglieder auf die Zusatzfelder erweitern</li>
+   	</ul>
+    <!-- 
+    $Log$
+    -->
+
+ <? include ("footer.inc"); ?>
+
