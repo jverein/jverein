781b955bec29da1a846ca602a37d90f0b440e408
jost <>
2010-11-15 21:29:31 +0000

    Leerzeichen werden ignoriert.

--- src/de/jost_net/JVerein/io/AltersgruppenParser.java
+++ src/de/jost_net/JVerein/io/AltersgruppenParser.java
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log$
+ * Revision 1.5  2010-10-15 09:58:28  jost
+ * Code aufgeräumt
+ *
  * Revision 1.4  2009-06-11 21:03:52  jost
  * Vorbereitung I18N
  *
@@ -30,6 +33,7 @@ import java.util.Vector;
 import org.eclipse.swt.graphics.Point;
 
 import de.jost_net.JVerein.JVereinPlugin;
+import de.willuhn.util.ApplicationException;
 
 public class AltersgruppenParser
 {
@@ -45,7 +49,8 @@ public class AltersgruppenParser
     Vector<String> gruppen = new Vector<String>();
     while (stt.hasMoreElements())
     {
-      gruppen.addElement(stt.nextToken());
+      String token = stt.nextToken().trim();
+      gruppen.addElement(token);
     }
     // Schritt 2: Zerlegen der Gruppen in ihre einzelnen Elemente
     elemente = new Vector<String>();
@@ -56,9 +61,9 @@ public class AltersgruppenParser
       {
         throw new RuntimeException(JVereinPlugin.getI18n().tr(
             "Ungültige Altersgruppe: {0}}",
-            new String[] { gruppen.elementAt(i)}));
+            new String[] { gruppen.elementAt(i) }));
       }
-      elemente.addElement(stt.nextToken());
+      elemente.addElement(stt.nextToken().trim());
       elemente.addElement(stt.nextToken());
     }
   }
@@ -75,16 +80,24 @@ public class AltersgruppenParser
     }
   }
 
-  public Point getNext()
+  public Point getNext() throws ApplicationException
   {
     Point p = new Point(getValue(), getValue());
     return p;
   }
 
-  private int getValue()
+  private int getValue() throws ApplicationException
   {
-    int value = Integer.parseInt(elemente.elementAt(ei));
-    ei++;
-    return value;
+    try
+    {
+      int value = Integer.parseInt(elemente.elementAt(ei).trim());
+      ei++;
+      return value;
+    }
+    catch (NumberFormatException e)
+    {
+      throw new ApplicationException("Fehler in den Altergruppen "
+          + e.getMessage());
+    }
   }
 }
