fef615fac3f13939414ef9e4f112a701f7a056b5
jost <>
2010-11-22 21:29:00 +0000

    *** empty log message ***

---
+++ web/arbeitseinsatz.php
@@ -0,0 +1,25 @@
+<? include ("frame.inc"); ?>
+    <h1>Arbeitseinsatz</h1>
+	<p>ab Version 1.5 verfügbar</p>
+	
+	<h2>Aktivierung</h2>
+	<p>Zunächst sind die Arbeitseinsätze zu aktivieren. Das geschieht unter Administration&gt;Einstellungen&gt;Anzeige</p>
+    <p>Anschließend muss JVerein neu gestartet werden.</p> 
+    
+    <h2>Arbeitseinsätze buchen</h2>
+    <p>Die Arbeitseinsätze werden direkt beim Mitglied gebucht dafür ist der Tab "Arbeitseinsatz" zu öffnen:</p>
+    <img src='images/ArbeitseinsatzMitglied.png' class='screenshot'>
+    <p>Mit einem Klick auf neu öffnet sich folgendes Formular:</p>
+    <img src='images/ArbeitseinsatzNeu.png' class='screenshot'>
+    
+    <h2>Arbeitseinsätze kontrollieren</h2>
+    <p>Die Arbeitseinsätze können ausgewertet werden. Dabei werden Minder-, Normal- und Mehrleistungen überprüft:</p>    
+    <img src='images/ArbeitseinsaetzeKontrollieren.png' class='screenshot'>
+    
+    
+    <!-- 
+    $Log$
+    -->
+    
+<? include ("footer.inc"); ?>
+
