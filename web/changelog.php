35ca8c7ae4213c31dccc7bd704447d1c72f74dc0
jost <>
2011-02-23 18:06:45 +0000

    *** empty log message ***

--- web/changelog.php
+++ web/changelog.php
@@ -34,6 +34,7 @@
 					<li>Bugfix: Bei den Spendenbescheinigungen wird der Betrag wie in der Vorschau positioniert.</li>
 					<li>Warnung bei der Neuaufnahme von Arbeitseinsätzen, Lehrgängen, Wiedervorlagen und Zusatzbeträgen, wenn das neue Mitglied noch nicht gespeichert ist.</li>
 					<li>Colins Patch zur MySQL-Performancesteigerung.</li>
+					<li>Neu: Kompakte Abbuchung. Zusammenfassung von mehreren Abbuchungen zu einer Bankverbindung.</li>
     		</ul>
 	   	<li>Version 1.4.0 vom 10.11.2010</li>
     		<ul>
@@ -557,6 +558,9 @@
 </ul>
     <!-- 
     $Log$
+    Revision 1.109  2011-02-15 20:59:28  jost
+    *** empty log message ***
+
     Revision 1.108  2011-02-12 22:53:16  jost
     *** empty log message ***
 
