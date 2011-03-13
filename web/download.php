11b211d4f6ffdc8c5375e80cd9f358695a1a55fe
jost <>
2011-03-13 18:30:30 +0000

    *** empty log message ***

--- web/download.php
+++ web/download.php
@@ -11,8 +11,8 @@
     		<p>Hinweis! In der Version 1.4.0.1 des Windows-Installers in ein Mini-Bug in Syntax beseitigt worden.</p>
     	<li>Entwicklerversionen</li>
     		<ul>
+    			<li><a href='download/download_log.php?dl=jverein.1.5.0-devel-271.zip'>Entwicklerversion 1.5.0 Rev. 271</a>&nbsp;Für alle Betriebssysteme. Vom 13.03.2011 / zusammen mit aktuellen Nightly-Builds von Jameica und Hibiscus einsetzen.</li>
     			<li><a href='download/download_log.php?dl=jverein.1.5.0-devel-269.zip'>Entwicklerversion 1.5.0 Rev. 269</a>&nbsp;Für alle Betriebssysteme. Vom 23.02.2011 / zusammen mit aktuellen Nightly-Builds von Jameica und Hibiscus einsetzen.</li>
-    			<li><a href='download/download_log.php?dl=jverein.1.5.0-devel-268.zip'>Entwicklerversion 1.5.0 Rev. 268</a>&nbsp;Für alle Betriebssysteme. Vom 30.01.2011 / zusammen mit aktuellen Nightly-Builds von Jameica und Hibiscus einsetzen.</li>
     		</ul>
     	<li>Vorherige Versionen</li>
     		<ul>
@@ -29,6 +29,9 @@
     
     <!-- 
     $Log$
+    Revision 1.23  2011-02-23 18:13:49  jost
+    *** empty log message ***
+
     Revision 1.22  2011-01-30 10:13:03  jost
     *** empty log message ***
 
