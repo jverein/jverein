06862a5456db7dbdb8518bd6b21f3888f0ab49f2
jost <>
2011-03-18 19:17:27 +0000

    *** empty log message ***

--- web/changelog.php
+++ web/changelog.php
@@ -52,6 +52,7 @@
 					<hr>
 					<li>Die Änderungen ab hier wurden nach Upload der Entwicklerversion 273 vorgenommen.</li>
 					<li>Die Boxen "aktuelle Geburtstage" und "Wiedervorlagen" für die Startseite entfernt. Ersatz ist die neue Terminübersicht.</li>
+					<li>Neue Box "Termine" für die Startseite erstellt</li>
     		</ul>
 	   	<li>Version 1.4.0 vom 10.11.2010</li>
     		<ul>
@@ -575,6 +576,9 @@
 </ul>
     <!-- 
     $Log$
+    Revision 1.116  2011-03-17 19:51:29  jost
+    Aktuelle Geburtstage und Wiedervorlage ausgemustert. Ersatz durch die neue Terminübersicht.
+
     Revision 1.115  2011-03-15 07:27:40  jost
     *** empty log message ***
 
