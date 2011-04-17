b8336b3960aa78e3a7fa24c556b108518ee1695c
jost <>
2011-04-17 17:58:17 +0000

    *** empty log message ***

--- web/dokumentationallinone.php
+++ web/dokumentationallinone.php
@@ -16,7 +16,6 @@
 	<? include ("abrechnung.php"); ?>
 	<? include ("zusatzbetraege.php"); ?>
 	<? include ("zusatzbetraege_import.php"); ?>
-	<? include ("manuelle_zahlungseingaenge.php"); ?>
 	<? include ("wiedervorlage.php"); ?>
 	<? include ("lehrgaenge.php"); ?>
 	<? include ("spendenbescheinigung.php"); ?>
@@ -43,6 +42,9 @@
 
     <!-- 
     $Log$
+    Revision 1.10  2011-04-17 17:56:45  jost
+    *** empty log message ***
+
     Revision 1.9  2010-11-03 11:57:37  jost
     *** empty log message ***
 
