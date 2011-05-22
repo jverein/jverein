1c5093117fee5f87a456a893ecaca88e3c4e572c
jost <>
2011-05-22 12:34:04 +0000

    Version 1.5

--- web/jameica_startseite.php
+++ web/jameica_startseite.php
@@ -1,7 +1,7 @@
 <? include ("frame.inc"); ?>
     <h1>Jameica Startseite</h1>
     <p>
-    In die Jameica-Startseite können 
+    In die Jameica-Startseite können bis zur Version 1.4
     <ul>
     	<li>die aktuellen Geburtstage</li>
     	<li>die Wiedervorlagetermine</li>
@@ -10,6 +10,10 @@
     
     Auf der Jameica-Seite können die Einstellungen unter 'Startseite anpassen' 
     <img src='images/JameicaStartseite.jpg' class='screenshot'>
+    
+    <p>
+    Ab Version 1.5 sieht der Einstellungsdialog so aus:</p> 
+    <img src='images/JameicaStartseite.PNG' class='screenshot'>
     
     
     <h2>Aktuelle Geburtstage</h2>
@@ -21,9 +25,18 @@
     <h2>Wiedervorlage</h2>
     <img src='images/WiedervorlageBox.jpg' class='screenshot'>
     
+    <h2>Terminübersicht (ab Version 1.5)</h2>
+    <p>Ab Version 1.5 werden die Geburtstage und die Wiedervorlagen in einem Kalender
+    angezeigt.</p>
+
+    <img src='images/Terminuebersicht.PNG' class='screenshot'>
+    
     
     <!-- 
     $Log$
+    Revision 1.2  2009/07/16 16:45:23  jost
+    Neu: Aktuelle Geburtstage
+
     Revision 1.1  2009/05/08 14:46:22  jost
     shtml - php
 
