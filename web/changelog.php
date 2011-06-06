8d586c49070a1f5cf38565c114effd00334d6471
jost <>
2011-06-06 19:22:37 +0000

    *** empty log message ***

--- web/changelog.php
+++ web/changelog.php
@@ -102,10 +102,14 @@
           <li>Jahressaldo: Bugfix abweichendes Geschäftsjahr.</li>
           <li>Neu: Bei der Erstellung von Rechnungen können Abbucher ausgeschlossen werden.</li>
           <li>Vereinheitlichung im Umgang mit den Variablen bei Formularen. Neue Variablennamen. Teilweise wurden die Werte konvertiert, teilweise muss von Hand nachgebessert werden. An den meisten Stellen stehen jetzt sämtliche Daten des Mitglieds, Eigenschaften und Zusatzfelder zur Verfügung.</li>
+          <hr>
           <li>Die Änderungen ab hier wurden nach Upload der Entwicklerversion 297 vorgenommen.</li>
           <li>Bei der Erfassung der Mails kann mit einem Rechtsklick auf eine Mail-Adresse ein Dialog mit einer Liste aller Variablen angezeigt werden.</li>
+          <hr>
+          <li>Die Änderungen ab hier wurden nach Upload der Entwicklerversion 298 vorgenommen.</li>
           <li>Zusätzliche Variablen: mitglied_anrede_foermlich und mitglied_anrede_du</li>
     		  <li>Abrechnung: Bugfix Textschlüssel bei Hibiscusbuchungen</li>
+    		  <li>Neu: Funktion zur gleichzeitigen Zuordnung einer Eigenschaft an viele Mitglieder</li>
     		</ul>
 	   	<li>Version 1.4.0 vom 10.11.2010</li>
     		<ul>
@@ -629,6 +633,9 @@
 </ul>
     <!-- 
     $Log$
+    Revision 1.156  2011-06-06 18:50:21  jost
+    *** empty log message ***
+
     Revision 1.155  2011-05-29 12:52:52  jost
     *** empty log message ***
 
