901d2b75454ae1799e92f98377044787634c41b7
jost <>
2011-06-06 19:17:13 +0000

    Kommentar entfernt

--- src/de/jost_net/JVerein/gui/dialogs/ZusatzfelderAuswahlDialog.java
+++ src/de/jost_net/JVerein/gui/dialogs/ZusatzfelderAuswahlDialog.java
@@ -9,6 +9,9 @@
  * heiner@jverein.de
  * www.jverein.de
  * $Log$
+ * Revision 1.2  2011-04-20 19:42:33  jost
+ * Stringfelder mit LIKE abfragen
+ *
  * Revision 1.1  2011-04-17 06:37:36  jost
  * Neu: Mitglieder-Selektion nach Zusatzfeldern
  *
@@ -49,14 +52,6 @@ public class ZusatzfelderAuswahlDialog extends AbstractDialog
 
   private Settings settings;
 
-  /**
-   * Eigenschaften oder Eigenschaftengruppen auswählen
-   * 
-   * @param modus
-   *          MODUS_EIGENSCHAFTEN oder MODUS_EIGENSCHAFTEN_UND_GRUPPEN
-   * @param defaults
-   *          Liste der Eigenschaften-IDs durch Komma separiert.
-   */
   public ZusatzfelderAuswahlDialog(Settings settings)
   {
     super(EigenschaftenAuswahlDialog.POSITION_CENTER);
