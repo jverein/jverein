87f1b4ea562a206960e975984a5a2c21648a167f
jost <>
2011-09-18 09:37:16 +0000

    *** empty log message ***

--- web/changelog.php
+++ web/changelog.php
@@ -38,6 +38,7 @@
           <li>Bei der Speicherung der Einstellungen wird auf notwendige Plugins geprüft, wenn Dokumentenspeicherung aktiviert wurde.</li>
           <li>Bugfix Sortierung Buchungsartenliste. Siehe <a href='http://www.jverein.de/forum/viewtopic.php?t=693&p=2709#p2709'>Forum</a></li>
           <li>Mitgliedskonto: Bei der Auswahl können jetzt nicht nur Mitglieder oder Spender sondern alle Adressen ausgewählt werden.</a>
+          <li>Mehreren Buchungen ein Mitgliedskonto gleichzeitig zuordnen. Siehe <a href='http://www.jverein.de/forum/viewtopic.php?f=1&t=699&p=2740'>Forum</a></li>
         </ul>
       </li>
       <li><a name="v201">Version 2.0.1 vom 29.6.2011</a></li>
