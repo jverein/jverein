8c1af6b2f4fc736f1683d35ea4e7337750bc2588
Heiner Jostkleigrewe <heiner@jost-net.de>
2011-10-16 12:57:39 +0000

    Umzug zu Sourceforge

--- web/download.php
+++ web/download.php
@@ -2,35 +2,33 @@
     <h1>Download</h1>
     <h2>Freigegebene Version</h2>
     		<ul>
-    			<li><a href='http://prdownload.berlios.de/jverein/jverein.2.0.1.zip'>Version 2.0.1</a>&nbsp;Für alle Betriebssysteme. Vom 30.06.2011 / zusammen mit Jameica >= 2.0.0 und Hibiscus >= 2.0.0 einsetzen.</li>
-          <li><a href='http://prdownload.berlios.de/jverein/JameicaSuitePortable.zip'>portable Jameica-Suite Version 2.0.1</a>&nbsp;Für Windows und Linux. Vom 1.7.2011 </li>
-          <li><a href='http://prdownload.berlios.de/jverein/jameicasuite-setup202_32.exe'>Jameica-Suite Version 2.0.2</a>&nbsp;Version 2.0.2</a>&nbsp;Windows 32-bit-Setup.</li>
-          <li><a href='http://prdownload.berlios.de/jverein/jameicasuite-setup202_64.exe'>Jameica-Suite Version 2.0.2</a>&nbsp;Version 2.0.2</a>&nbsp;Windows 64-bit-Setup.</li>
+    			<li><a href='http://sourceforge.net/projects/jverein/files/aktuelleVersion/jverein.2.0.1.zip/download'>Version 2.0.1</a>&nbsp;Für alle Betriebssysteme. Vom 30.06.2011 / zusammen mit Jameica >= 2.0.0 und Hibiscus >= 2.0.0 einsetzen.</li>
+          <li><a href='http://sourceforge.net/projects/jverein/files/aktuelleVersion/JameicaSuitePortable.zip/download'>portable Jameica-Suite Version 2.0.1</a>&nbsp;Für Windows und Linux. Vom 1.7.2011 </li>
+          <li><a href='http://sourceforge.net/projects/jverein/files/aktuelleVersion/jameicasuite-setup202_32.exe/download'>Jameica-Suite Version 2.0.2</a>&nbsp;Version 2.0.2</a>&nbsp;Windows 32-bit-Setup.</li>
+          <li><a href='http://sourceforge.net/projects/jverein/files/aktuelleVersion/jameicasuite-setup202_64.exe/download'>Jameica-Suite Version 2.0.2</a>&nbsp;Version 2.0.2</a>&nbsp;Windows 64-bit-Setup.</li>
     		</ul>
     <h2>Entwicklerversionen</h2>
      		<ul>
-          <li><a href='http://prdownload.berlios.de/jverein/jverein.2.1.0-devel-315.zip'>Version 2.1 Rev. 315</a>&nbsp;Für alle Betriebssysteme. Vom 27.08.2011 / zusammen mit aktuellen Nighly-Builds von Jameica und Hibiscus einsetzen.</li>
-          <li><a href='http://prdownload.berlios.de/jverein/jverein.2.1.0-devel-312.zip'>Version 2.1 Rev. 312</a>&nbsp;Für alle Betriebssysteme. Vom 07.08.2011 / zusammen mit aktuellen Nighly-Builds von Jameica und Hibiscus einsetzen.</li>
+          <li><a href='http://sourceforge.net/projects/jverein/files/Entwicklerversionen/jverein.2.1.0-devel-319.zip/download'>Version 2.1 Rev. 319</a>&nbsp;Für alle Betriebssysteme. Vom 27.08.2011 / zusammen mit aktuellen Nighly-Builds von Jameica und Hibiscus einsetzen.</li>
+          <li><a href='http://sourceforge.net/projects/jverein/files/Entwicklerversionen/jverein.2.1.0-devel-315.zip/download'>Version 2.1 Rev. 315</a>&nbsp;Für alle Betriebssysteme. Vom 07.08.2011 / zusammen mit aktuellen Nighly-Builds von Jameica und Hibiscus einsetzen.</li>
     		</ul> 
     <h2>Vorherige Versionen</h2>
     		<ul>
-          <li><a href='download/download_log.php?dl=jverein.2.0.0.zip'>Version 2.0.0</a>&nbsp;Für alle Betriebssysteme. Vom 23.06.2011 / zusammen mit Jameica 2.0.0 und Hibiscus 2.0.0 einsetzen.</li>
-          <li><a href='http://prdownload.berlios.de/jameica-suites/jameicasuite-setup1403_32.exe'>Version 1.4.0.1 32 bit</a>&nbsp;Windows 32-bit-Setup.</li>
-          <li><a href='http://prdownload.berlios.de/jameica-suites/jameicasuite-setup1403_64.exe'>Version 1.4.0.1 64 bit</a>&nbsp;Windows 64-bit-Setup.</li>
-    			<li><a href='download/download_log.php?dl=jverein.1.4.0.zip'>Version 1.4.0</a>&nbsp;Für alle Betriebssysteme. Vom 10.11.2010 / zusammen mit Jameica 1.10 und Hibiscus 1.12 einsetzen.</li>
-    			<li><a href='download/download_log.php?dl=jverein.1.3.3.zip'>Version 1.3.3</a>&nbsp;Für alle Betriebssysteme. Vom 31.5.2010 / zusammen mit Jameica 1.9 und Hibiscus 1.11 einsetzen.</li>
-    			<li><a href='download/download_log.php?dl=jameicasuite-setup133_32.exe'>Version 1.3.3</a>&nbsp;Windows 32-bit-Setup.</li>
-    			<li><a href='download/download_log.php?dl=jameicasuite-setup133_64.exe'>Version 1.3.3</a>&nbsp;Windows 64-bit-Setup.</li>
-    			<li><a href='download/download_log.php?dl=jverein.1.3.2.zip'>Version 1.3.2</a>vom 18.5.2010 / zusammen mit Jameica 1.9 und Hibiscus 1.11 einsetzen </li>
-    			<li><a href='download/download_log.php?dl=jverein.1.3.1.zip'>Version 1.3.1</a>&nbsp;vom 16.5.2010 / zusammen mit Jameica 1.9 und Hibiscus 1.11 einsetzen </li>
-    			<li><a href='download/download_log.php?dl=jverein.1.3.0.zip'>Version 1.3.0</a>&nbsp;vom 9.4.2010 / zusammen mit Jameica 1.9 und Hibiscus 1.11 einsetzen </li>
-    			<li><a href='download/download_log.php?dl=jverein.1.2.0.zip'>Version 1.2.0</a>&nbsp;vom 23.8.2009 / zusammen mit Jameica 1.8 und Hibiscus 1.10 einsetzen </li>
+    			<li><a href='http://sourceforge.net/projects/jverein/files/Vorgaengerversionen/jverein.1.4.0.zip/download'>Version 1.4.0</a>&nbsp;Für alle Betriebssysteme. Vom 10.11.2010 / zusammen mit Jameica 1.10 und Hibiscus 1.12 einsetzen.</li>
+    			<li><a href='http://sourceforge.net/projects/jverein/files/Vorgaengerversionen/jverein.1.3.3.zip/download'>Version 1.3.3</a>&nbsp;Für alle Betriebssysteme. Vom 31.5.2010 / zusammen mit Jameica 1.9 und Hibiscus 1.11 einsetzen.</li>
+    			<li><a href='http://sourceforge.net/projects/jverein/files/Vorgaengerversionen/jverein.1.3.2.zip/download'>Version 1.3.2</a>vom 18.5.2010 / zusammen mit Jameica 1.9 und Hibiscus 1.11 einsetzen </li>
+    			<li><a href='http://sourceforge.net/projects/jverein/files/Vorgaengerversionen/jverein.1.3.1.zip/download'>Version 1.3.1</a>&nbsp;vom 16.5.2010 / zusammen mit Jameica 1.9 und Hibiscus 1.11 einsetzen </li>
+    			<li><a href='http://sourceforge.net/projects/jverein/files/Vorgaengerversionen/jverein.1.3.0.zip/download'>Version 1.3.0</a>&nbsp;vom 9.4.2010 / zusammen mit Jameica 1.9 und Hibiscus 1.11 einsetzen </li>
+    			<li><a href='http://sourceforge.net/projects/jverein/files/Vorgaengerversionen/jverein.1.2.0.zip/download'>Version 1.2.0</a>&nbsp;vom 23.8.2009 / zusammen mit Jameica 1.8 und Hibiscus 1.10 einsetzen </li>
     		</ul>
     </ul>
     
     
     <!-- 
     $Log$
+    Revision 1.48  2011/08/27 11:13:40  jost
+    *** empty log message ***
+
     Revision 1.47  2011-08-12 15:57:17  jost
     *** empty log message ***
 
