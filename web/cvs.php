b08eca91e909ef24aa94b6ad54320fe3609483a6
Heiner Jostkleigrewe <heiner@jost-net.de>
2011-10-16 12:37:29 +0000

    Umzug zu Sourceforge

--- web/cvs.php
+++ web/cvs.php
@@ -1,10 +1,13 @@
 <? include ("frame.inc"); ?>
     <h1>CVS</h1>
     <p>Zugriff auf die aktuelle Entwicklerversion gibt es auf der
-    <a href="http://developer.berlios.de/cvs/?group_id=7335" target="_blank">Projekt-Seite</a> über das CVS.</p>
+    <a href="http://sourceforge.net/projects/jverein/develop" target="_blank">Projekt-Seite</a> über das CVS.</p>
        
     <!-- 
     $Log$
+    Revision 1.1  2009/05/08 14:46:22  jost
+    shtml - php
+
     Revision 1.4  2009/05/03 15:33:30  jost
     Neue Homepage
 
