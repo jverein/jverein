b49606f8657dee3431cfe46ef77ff98cfae6d30f
Heiner Jostkleigrewe <heiner@jost-net.de>
2011-11-20 07:27:22 +0000

    Vorbereitung f�r Buchungsexport

---
+++ src/de/jost_net/JVerein/Variable/BuchungVar.java
@@ -0,0 +1,56 @@
+/**********************************************************************
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * Copyright (c) by Heiner Jostkleigrewe
+ * This program is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without 
+ *  even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+ *  the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.  If not, 
+ * see <http://www.gnu.org/licenses/>.
+ * 
+ * heiner@jverein.de
+ * www.jverein.de
+ **********************************************************************/
+package de.jost_net.JVerein.Variable;
+
+public enum BuchungVar
+{
+  ABRECHNUNGSLAUF("buchung_abrechnungslauf"), //
+  ART("buchung_art"), //
+  AUSZUGSNUMMER("buchung_auszugsnummer"), //
+  BETRAG("buchung_betrag"), //
+  BLATTNUMMER("buchung_blattnummer"), //
+  BUCHUNGSARBEZEICHNUNG("buchung_buchungsart_bezeichnung"), //
+  BUCHUNGSARTNUMMER("buchung_buchungsart_nummer"), //
+  BUCHUNGSKLASSEBEZEICHNUNG("buchung_buchungsklasse_bezeichnung"), //
+  BUCHUNGSKLASSENUMMER("buchung_buchungsklasse_nummer"), //
+  DATUM("buchung_datum"), //
+  JAHRESABSCHLUSS("buchung_jahresabschluss"), //
+  KOMMENTAR("buchung_kommentar"), //
+  KONTONUMMER("buchung_kontonummer"), //
+  MITGLIEDSKONTO("buchung_mitgliedskonto"), //
+  NAME("buchung_name"), //
+  SPENDENBESCHEINIGUNG("buchung_spendenbescheinigung"), // ;
+  ZWECK1("buchung_zweck1"), //
+  ZWECK2("buchung_zweck2");
+
+  private String name;
+
+  BuchungVar(String name)
+  {
+    this.name = name;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+}
