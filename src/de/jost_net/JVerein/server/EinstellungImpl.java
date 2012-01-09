70ac5577329a6d99b319e479526709c09a171ccc
Heiner Jostkleigrewe <heiner@jost-net.de>
2012-01-09 19:13:20 +0000

    Bugfix Dateinamenmuster

--- src/de/jost_net/JVerein/server/EinstellungImpl.java
+++ src/de/jost_net/JVerein/server/EinstellungImpl.java
@@ -600,7 +600,7 @@ public class EinstellungImpl extends AbstractDBObject implements Einstellung
     String muster = (String) getAttribute("dateinamenmusterspende");
     if (muster == null)
     {
-      muster = "a$-d$-z$-$n-$v";
+      muster = "a$-d$-n$-v$";
     }
     return muster;
   }
