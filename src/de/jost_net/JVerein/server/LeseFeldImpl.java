12a79eab1d1fc603fa91ec37c5c6deca610609b7
Heiner Jostkleigrewe <heiner@jost-net.de>
2012-02-27 18:13:33 +0000

    *** empty log message ***

---
+++ src/de/jost_net/JVerein/server/LeseFeldImpl.java
@@ -0,0 +1,111 @@
+/**********************************************************************
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * Copyright (c) by Heiner Jostkleigrewe
+ * This program is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without 
+ *  even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+ *  the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.  If not, 
+ * see <http://www.gnu.org/licenses/>.
+ * 
+ * heiner@jverein.de
+ * www.jverein.de
+ **********************************************************************/
+package de.jost_net.JVerein.server;
+
+import java.rmi.RemoteException;
+
+import de.jost_net.JVerein.JVereinPlugin;
+import de.jost_net.JVerein.rmi.LeseFeld;
+import de.willuhn.datasource.db.AbstractDBObject;
+import de.willuhn.logging.Logger;
+import de.willuhn.util.ApplicationException;
+
+public class LeseFeldImpl extends AbstractDBObject implements LeseFeld
+{
+  private static final long serialVersionUID = 1610434197155984031L;
+
+  public LeseFeldImpl() throws RemoteException
+  {
+    super();
+  }
+
+  @Override
+  protected String getTableName()
+  {
+    return "lesefeld";
+  }
+
+  @Override
+  public String getPrimaryAttribute()
+  {
+    return "id";
+  }
+
+  @Override
+  protected void insertCheck() throws ApplicationException
+  {
+    try
+    {
+      if (getScript() == null || getScript().length() == 0)
+      {
+        throw new ApplicationException(JVereinPlugin.getI18n().tr(
+            "Bitte gültiges Script eingeben"));
+      }
+
+    }
+    catch (RemoteException e)
+    {
+      String fehler = JVereinPlugin.getI18n().tr(
+          "Lesefeld kann nicht gespeichert werden. Siehe system log");
+      Logger.error(fehler, e);
+      throw new ApplicationException(fehler);
+    }
+  }
+
+  @Override
+  protected void updateCheck() throws ApplicationException
+  {
+    insertCheck();
+  }
+
+  @Override
+  protected Class<?> getForeignObject(String arg0)
+  {
+    return null;
+  }
+
+  public String getScript() throws RemoteException
+  {
+    return (String) getAttribute("script");
+  }
+
+  public void setScript(String script) throws RemoteException
+  {
+    setAttribute("script", script);
+  }
+
+  public String getBezeichnung() throws RemoteException
+  {
+    return (String) getAttribute("bezeichnung");
+  }
+
+  public void setBezeichnung(String bezeichnung) throws RemoteException
+  {
+    setAttribute("bezeichnung", bezeichnung);
+  }
+
+  @Override
+  public Object getAttribute(String fieldName) throws RemoteException
+  {
+    return super.getAttribute(fieldName);
+  }
+}
