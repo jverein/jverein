94418fdfb49e130f6eb44a1d5ccc255f9a13f512
Heiner Jostkleigrewe <heiner@jost-net.de>
2012-03-04 07:41:59 +0000

    *** empty log message ***

--- web/changelog.php
+++ web/changelog.php
@@ -3,15 +3,19 @@
     <ul>      
       <li><a name="v230">Version 2.3.0 (noch nicht freigegeben)</a></li>
         <ul>
-          <li>Bugfix bei der Neuaufnahme von neuen Mitgliedern wenn die Option "juristische Personen" aktiviert wurde. Siehe <a href="http://www.jverein.de/forum/viewtopic.php?f=5&t=940&p=3742">Forum</a> [2.2.1]</li>
-          <li>Bugfix Abrechung Zusatzbeträge bei Austrittsdatum in der Zukunft. Siehe <a href="http://www.jverein.de/forum/viewtopic.php?f=5&t=950&p=3732">Forum</a>[2.2.1]</li>
           <li>Bugfix: Fehlendes Konto bei 1. Buchung. Siehe <a href="http://www.jverein.de/forum/viewtopic.php?f=5&t=946">Forum</a></li>
           <li>Bugfix: Übersicht Lehrgänge. Die Such-Lehrgangsart wurde immer wieder zurückgesetzt.</li>
           <li>Neu: Import von Buchungen. Siehe <a href="http://www.jverein.de/buchung_import.php">Dokumentation.</a></li>
-          <li>Bugfix Personalbogen. Scalierung des Bildes unter Windows entfernt. Siehe <a href='http://www.jverein.de/forum/viewtopic.php?f=5&t=237&p=3724#p3724'>Forum</a>[2.2.1]</li>
           <li>Mitglieder-Suche: Tabs entfernt. Gründe: Unterschiedliches Verhalten der Tabellen innerhalb der Tabs und Performance-Optimierung.</li>
-          <li>Bugfix Import: Bei fehlender Spalte Adresstyp wurde der Zahlungsrhytmus überschrieben. Siehe <a href="http://www.jverein.de/forum/viewtopic.php?f=5&t=417&p=3655">Forum</a> [2.2.1]</li>
-          <li>Bugfix Formularfeld "Anrede_du": Komma fehlte am Ende. Siehe <a href="http://www.jverein.de/forum/viewtopic.php?f=5&t=934">Forum</a>[2.2.1]</li>
+         </ul>
+      </li>
+      <li><a name="v221">Version 2.2.1 vom 04.03.2012</a></li>
+        <ul>
+          <li>Bugfix bei der Neuaufnahme von neuen Mitgliedern wenn die Option "juristische Personen" aktiviert wurde. Siehe <a href="http://www.jverein.de/forum/viewtopic.php?f=5&t=940&p=3742">Forum</a></li>
+          <li>Bugfix Abrechung Zusatzbeträge bei Austrittsdatum in der Zukunft. Siehe <a href="http://www.jverein.de/forum/viewtopic.php?f=5&t=950&p=3732">Forum</a></li>
+          <li>Bugfix Personalbogen. Scalierung des Bildes unter Windows entfernt. Siehe <a href='http://www.jverein.de/forum/viewtopic.php?f=5&t=237&p=3724#p3724'>Forum</a></li>
+          <li>Bugfix Import: Bei fehlender Spalte Adresstyp wurde der Zahlungsrhytmus überschrieben. Siehe <a href="http://www.jverein.de/forum/viewtopic.php?f=5&t=417&p=3655">Forum</a></li>
+          <li>Bugfix Formularfeld "Anrede_du": Komma fehlte am Ende. Siehe <a href="http://www.jverein.de/forum/viewtopic.php?f=5&t=934">Forum</a></li>
          </ul>
       </li>
     
