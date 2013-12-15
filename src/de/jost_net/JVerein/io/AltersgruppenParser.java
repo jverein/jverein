f607620e3ab0c2e02f34add0d28d8bbdd428b4aa
Heiner <heiner@jost-net.de>
2013-12-15 08:04:23 +0100

    Check auf Null

--- src/de/jost_net/JVerein/io/AltersgruppenParser.java
+++ src/de/jost_net/JVerein/io/AltersgruppenParser.java
@@ -1,9 +1,4 @@
 /**********************************************************************
- * $Source$
- * $Revision$
- * $Date$
- * $Author$
- *
  * Copyright (c) by Heiner Jostkleigrewe
  * This program is free software: you can redistribute it and/or modify it under the terms of the 
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the 
@@ -38,6 +33,10 @@ public class AltersgruppenParser
 
   public AltersgruppenParser(String altersgruppe) throws RuntimeException
   {
+    if (altersgruppe == null)
+    {
+      throw new RuntimeException("Altersgruppe(n) müssen gefüllt sein!");
+    }
     // Schritt 1: Zerlegen in die einzelnen Gruppen
     StringTokenizer stt = new StringTokenizer(altersgruppe, ",");
     Vector<String> gruppen = new Vector<String>();
