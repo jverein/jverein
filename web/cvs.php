b9f8ee626eb790874d82fba2897329b9a513bd47
Heiner <heiner@jost-net.de>
2013-12-23 17:41:47 +0100

    Alten Web-Auftritt gelöscht.

--- web/cvs.php
+++
@@ -1,8 +0,0 @@
-<? include ("frame.inc"); ?>
-    <h1>CVS</h1>
-    <p>Zugriff auf die aktuelle Entwicklerversion gibt es auf der
-    <a href="http://sourceforge.net/projects/jverein/develop" target="_blank">Projekt-Seite</a> über das CVS.</p>
-       
-       
-<? include ("footer.inc"); ?>
-
--- web/kontakt.php
+++
@@ -1,7 +0,0 @@
-<? include ("frame.inc"); ?>
-    <h2>Kontakt</h2>
-    <p>Anregungen, Erweiterungswünsche und (möglichst keine) Fehlermeldungen bitte in
-       das <a href="forum">Forum</a> posten.</p>
-       
-<? include ("footer.inc"); ?>
-
