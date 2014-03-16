d5ad6482e51d57b51ba8de51a434e9654bf018f1
Heiner <heiner@jost-net.de>
2014-03-16 07:46:23 +0100

    Eingabedatum bei Umwandlung von Adresse zu Mitglied setzen https://sourceforge.net/p/jverein/tickets/72/

--- src/de/jost_net/JVerein/gui/menu/MitgliedMenu.java
+++ src/de/jost_net/JVerein/gui/menu/MitgliedMenu.java
@@ -1,9 +1,4 @@
 /**********************************************************************
- * $Source$
- * $Revision$
- * $Date$
- * $Author$
- *
  * Copyright (c) by Heiner Jostkleigrewe
  * This program is free software: you can redistribute it and/or modify it under the terms of the 
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the 
@@ -94,6 +89,7 @@ public class MitgliedMenu extends ContextMenu
               Logger.error("Fehler", e);
             }
             m.setAdresstyp(1);
+            m.setEingabedatum();
             GUI.startView(MitgliedDetailView.class.getName(), m);
           }
           catch (RemoteException e)
