1b2b30e8c682e2f25457aac5b51084c9aa5debb9
Heiner <heiner@jost-net.de>
2016-03-05 20:05:44 +0100

    Neue Variable vorjahr. Patch von Danzelot https://sourceforge.net/p/jverein/tickets/266/

--- src/de/jost_net/JVerein/Variable/AllgemeineMap.java
+++ src/de/jost_net/JVerein/Variable/AllgemeineMap.java
@@ -1,16 +1,16 @@
 /**********************************************************************
  * Copyright (c) by Heiner Jostkleigrewe
- * This program is free software: you can redistribute it and/or modify it under the terms of the 
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without 
- *  even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+ *  This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without
+ *  even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
  *  the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program.  If not, 
+ * You should have received a copy of the GNU General Public License along with this program.  If not,
  * see <http://www.gnu.org/licenses/>.
- * 
+ *
  * heiner@jverein.de
  * www.jverein.de
  **********************************************************************/
@@ -53,15 +53,23 @@ public class AllgemeineMap
     map.put(AllgemeineVar.TAGESDATUM.getName(), ttmmjjjj.format(new Date()));
     map.put(AllgemeineVar.AKTUELLESJAHR.getName(), jjjj.format(new Date()));
     map.put(AllgemeineVar.AKTUELLERMONAT.getName(), mmjjjj.format(new Date()));
+
     Calendar calendar = Calendar.getInstance();
     calendar.add(Calendar.MONTH, 1);
-    map.put(AllgemeineVar.FOLGEMONAT.getName(),
-        mmjjjj.format(calendar.getTime()));
-    calendar.add(Calendar.MONTH, -2);
-    map.put(AllgemeineVar.VORMONAT.getName(), mmjjjj.format(calendar.getTime()));
+    map.put(AllgemeineVar.FOLGEMONAT.getName(), mmjjjj.format(calendar.getTime()));
+
+    calendar = Calendar.getInstance();
     calendar.add(Calendar.MONTH, -1);
+    map.put(AllgemeineVar.VORMONAT.getName(), mmjjjj.format(calendar.getTime()));
+
+    calendar = Calendar.getInstance();
     calendar.add(Calendar.YEAR, 1);
     map.put(AllgemeineVar.FOLGEJAHR.getName(), jjjj.format(calendar.getTime()));
+
+    calendar = Calendar.getInstance();
+    calendar.add(Calendar.YEAR, -1);
+    map.put(AllgemeineVar.VORJAHR.getName(), jjjj.format( calendar.getTime()));
+
     return map;
   }
 }
