7a64b75ef6e1babd5fb96a9367f6976375572232
Heiner <heiner@jost-net.de>
2023-02-28 10:53:21 +0100

    https://github.com/openjverein/jverein/pull/16 Spendebesch 4 J.

--- src/de/jost_net/JVerein/gui/control/SpendenbescheinigungAutoNeuControl.java
+++ src/de/jost_net/JVerein/gui/control/SpendenbescheinigungAutoNeuControl.java
@@ -47,165 +47,172 @@ import de.willuhn.util.ApplicationException;
 public class SpendenbescheinigungAutoNeuControl extends AbstractControl
 {
 
-  private de.willuhn.jameica.system.Settings settings;
+	private de.willuhn.jameica.system.Settings settings;
 
-  private SelectInput jahr;
+	private SelectInput jahr;
 
-  private TreePart spbTree;
+	private TreePart spbTree;
 
-  private SelectInput formularEinzel;
+	private SelectInput formularEinzel;
 
-  private SelectInput formularSammel;
+	private SelectInput formularSammel;
 
-  public SpendenbescheinigungAutoNeuControl(AbstractView view)
-  {
-    super(view);
-    settings = new de.willuhn.jameica.system.Settings(this.getClass());
-    settings.setStoreWhenRead(true);
-  }
+	public SpendenbescheinigungAutoNeuControl(AbstractView view)
+	{
+		super(view);
+		settings = new de.willuhn.jameica.system.Settings(this.getClass());
+		settings.setStoreWhenRead(true);
+	}
 
-  public SelectInput getJahr()
-  {
-    if (jahr != null)
-    {
-      return jahr;
-    }
-    Calendar cal = Calendar.getInstance();
-    jahr = new SelectInput(
-        new Object[] { cal.get(Calendar.YEAR), cal.get(Calendar.YEAR) - 1 },
-        cal.get(Calendar.YEAR));
-    jahr.addListener(new Listener()
-    {
+	public SelectInput getJahr()
+	{
+		if (jahr != null)
+		{
+			return jahr;
+		}
+		Calendar cal = Calendar.getInstance();
+		jahr = new SelectInput(new Object[]
+		{
+				cal.get(Calendar.YEAR), cal.get(Calendar.YEAR) - 1,
+				cal.get(Calendar.YEAR) - 2, cal.get(Calendar.YEAR) - 3
+		}, cal.get(Calendar.YEAR));
+		jahr.addListener(new Listener()
+		{
 
-      @Override
-      public void handleEvent(Event event)
-      {
-        try
-        {
-          spbTree.setRootObject(
-              new SpendenbescheinigungNode((Integer) getJahr().getValue()));
-        }
-        catch (RemoteException e)
-        {
-          Logger.error("Fehler", e);
-        }
-      }
+			@Override
+			public void handleEvent(Event event)
+			{
+				try
+				{
+					spbTree.setRootObject(
+							new SpendenbescheinigungNode((Integer) getJahr().getValue()));
+				}
+				catch (RemoteException e)
+				{
+					Logger.error("Fehler", e);
+				}
+			}
 
-    });
-    return jahr;
-  }
+		});
+		return jahr;
+	}
 
-  public SelectInput getFormular() throws RemoteException
-  {
-    if (formularEinzel != null)
-    {
-      return formularEinzel;
-    }
-    DBIterator<Formular> it = Einstellungen.getDBService()
-        .createList(Formular.class);
-    it.addFilter("art = ?",
-        new Object[] { FormularArt.SPENDENBESCHEINIGUNG.getKey() });
-    formularEinzel = new SelectInput(it, null);
-    return formularEinzel;
-  }
+	public SelectInput getFormular() throws RemoteException
+	{
+		if (formularEinzel != null)
+		{
+			return formularEinzel;
+		}
+		DBIterator<
+				Formular> it = Einstellungen.getDBService().createList(Formular.class);
+		it.addFilter("art = ?", new Object[]
+		{
+				FormularArt.SPENDENBESCHEINIGUNG.getKey()
+		});
+		formularEinzel = new SelectInput(it, null);
+		return formularEinzel;
+	}
 
-  public SelectInput getFormularSammelbestaetigung() throws RemoteException
-  {
-    if (formularSammel != null)
-    {
-      return formularSammel;
-    }
-    DBIterator<Formular> it = Einstellungen.getDBService()
-        .createList(Formular.class);
-    it.addFilter("art = ?",
-        new Object[] { FormularArt.SAMMELSPENDENBESCHEINIGUNG.getKey() });
-    formularSammel = new SelectInput(it, null);
-    return formularSammel;
-  }
+	public SelectInput getFormularSammelbestaetigung() throws RemoteException
+	{
+		if (formularSammel != null)
+		{
+			return formularSammel;
+		}
+		DBIterator<
+				Formular> it = Einstellungen.getDBService().createList(Formular.class);
+		it.addFilter("art = ?", new Object[]
+		{
+				FormularArt.SAMMELSPENDENBESCHEINIGUNG.getKey()
+		});
+		formularSammel = new SelectInput(it, null);
+		return formularSammel;
+	}
 
-  /**
-   * This method stores the project using the current values.
-   */
-  public void handleStore()
-  {
-    //
-  }
+	/**
+	 * This method stores the project using the current values.
+	 */
+	public void handleStore()
+	{
+		//
+	}
 
-  public Button getSpendenbescheinigungErstellenButton()
-  {
-    Button b = new Button("erstellen", new Action()
-    {
+	public Button getSpendenbescheinigungErstellenButton()
+	{
+		Button b = new Button("erstellen", new Action()
+		{
 
-      @Override
-      public void handleAction(Object context) throws ApplicationException
-      {
-        try
-        {
-          @SuppressWarnings("rawtypes")
-          List items = spbTree.getItems();
-          SpendenbescheinigungNode spn = (SpendenbescheinigungNode) items
-              .get(0);
-          // Loop über die Mitglieder
-          GenericIterator it1 = spn.getChildren();
-          while (it1.hasNext())
-          {
-            SpendenbescheinigungNode sp1 = (SpendenbescheinigungNode) it1
-                .next();
-            Spendenbescheinigung spbescheinigung = (Spendenbescheinigung) Einstellungen
-                .getDBService().createObject(Spendenbescheinigung.class, null);
-            spbescheinigung.setSpendenart(Spendenart.GELDSPENDE);
-            spbescheinigung.setMitglied(sp1.getMitglied());
-            spbescheinigung.setZeile1(sp1.getMitglied().getAnrede());
-            spbescheinigung.setZeile2(
-                Adressaufbereitung.getVornameName(sp1.getMitglied()));
-            spbescheinigung.setZeile3(sp1.getMitglied().getStrasse());
-            spbescheinigung.setZeile4(
-                sp1.getMitglied().getPlz() + " " + sp1.getMitglied().getOrt());
-            spbescheinigung.setErsatzAufwendungen(false);
-            spbescheinigung.setBescheinigungsdatum(new Date());
-            spbescheinigung.setSpendedatum(new Date());
-            spbescheinigung.setBetrag(0.01);
-            spbescheinigung.setAutocreate(Boolean.TRUE);
-            // Loop über die Buchungen eines Mitglieds
-            GenericIterator it2 = sp1.getChildren();
-            while (it2.hasNext())
-            {
-              SpendenbescheinigungNode sp2 = (SpendenbescheinigungNode) it2
-                  .next();
-              spbescheinigung.addBuchung(sp2.getBuchung());
-            }
-            // Nun noch das korrekte Formular setzen
-            if (spbescheinigung.getBuchungen().size() > 1)
-            {
-              spbescheinigung.setFormular(
-                  (Formular) getFormularSammelbestaetigung().getValue());
-            }
-            else
-            {
-              spbescheinigung.setFormular((Formular) getFormular().getValue());
-            }
-            spbescheinigung.store();
-          }
-          GUI.getStatusBar()
-              .setSuccessText("Spendenbescheinigung(en) erstellt");
-          spbTree.removeAll();
-        }
-        catch (RemoteException e)
-        {
-          Logger.error(e.getMessage());
-          throw new ApplicationException(
-              "Fehler bei der Aufbereitung der Spendenbescheinigung");
-        }
-      }
-    }, null, false, "save.png");
-    return b;
-  }
+			@Override
+			public void handleAction(Object context) throws ApplicationException
+			{
+				try
+				{
+					@SuppressWarnings("rawtypes")
+					List items = spbTree.getItems();
+					SpendenbescheinigungNode spn = (SpendenbescheinigungNode) items
+							.get(0);
+					// Loop über die Mitglieder
+					GenericIterator it1 = spn.getChildren();
+					while (it1.hasNext())
+					{
+						SpendenbescheinigungNode sp1 = (SpendenbescheinigungNode) it1
+								.next();
+						Spendenbescheinigung spbescheinigung = (Spendenbescheinigung) Einstellungen
+								.getDBService().createObject(Spendenbescheinigung.class, null);
+						spbescheinigung.setSpendenart(Spendenart.GELDSPENDE);
+						spbescheinigung.setMitglied(sp1.getMitglied());
+						spbescheinigung.setZeile1(sp1.getMitglied().getAnrede());
+						spbescheinigung.setZeile2(
+								Adressaufbereitung.getVornameName(sp1.getMitglied()));
+						spbescheinigung.setZeile3(sp1.getMitglied().getStrasse());
+						spbescheinigung.setZeile4(
+								sp1.getMitglied().getPlz() + " " + sp1.getMitglied().getOrt());
+						spbescheinigung.setZeile5(sp1.getMitglied().getStaat());
+						spbescheinigung.setErsatzAufwendungen(false);
+						spbescheinigung.setBescheinigungsdatum(new Date());
+						spbescheinigung.setSpendedatum(new Date());
+						spbescheinigung.setBetrag(0.01);
+						spbescheinigung.setAutocreate(Boolean.TRUE);
+						// Loop über die Buchungen eines Mitglieds
+						GenericIterator it2 = sp1.getChildren();
+						while (it2.hasNext())
+						{
+							SpendenbescheinigungNode sp2 = (SpendenbescheinigungNode) it2
+									.next();
+							spbescheinigung.addBuchung(sp2.getBuchung());
+						}
+						// Nun noch das korrekte Formular setzen
+						if (spbescheinigung.getBuchungen().size() > 1)
+						{
+							spbescheinigung.setFormular(
+									(Formular) getFormularSammelbestaetigung().getValue());
+						}
+						else
+						{
+							spbescheinigung.setFormular((Formular) getFormular().getValue());
+						}
+						spbescheinigung.store();
+					}
+					GUI.getStatusBar()
+							.setSuccessText("Spendenbescheinigung(en) erstellt");
+					spbTree.removeAll();
+				}
+				catch (RemoteException e)
+				{
+					Logger.error(e.getMessage());
+					throw new ApplicationException(
+							"Fehler bei der Aufbereitung der Spendenbescheinigung");
+				}
+			}
+		}, null, false, "save.png");
+		return b;
+	}
 
-  public Part getSpendenbescheinigungTree() throws RemoteException
-  {
-    spbTree = new TreePart(
-        new SpendenbescheinigungNode((Integer) getJahr().getValue()), null);
-    return spbTree;
-  }
+	public Part getSpendenbescheinigungTree() throws RemoteException
+	{
+		spbTree = new TreePart(
+				new SpendenbescheinigungNode((Integer) getJahr().getValue()), null);
+		return spbTree;
+	}
 
 }
